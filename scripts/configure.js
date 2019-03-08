dde2e92809f9ddcd7f8920e5806c5cb1504fbc24
Mike Budzynski <mike.budzynski@microsoft.com>
2019-03-07 21:18:52 -0800

    Scripts for automating configuration and clean deployments

---
+++ scripts/configure.js
@@ -0,0 +1,63 @@
+var fs = require('fs'),
+    path = require('path'),    
+    configDesignFile = path.join(__dirname, '\\..\\src\\config.design.json'),
+    configPublishFile = path.join(__dirname, '\\..\\src\\config.publish.json'),
+    configRuntimeFile = path.join(__dirname, '\\..\\src\\config.runtime.json');
+
+var apimServiceValue = process.argv[2];
+var apimSasAccessTokenValue = process.argv[3];
+var storageSasUrlValue = process.argv[4];
+var storageConnectionStringValue = process.argv[5];
+
+var apimServiceUrlValue = `https://${apimServiceValue}.management.azure-api.net`;
+
+const apimServiceParameter = "managementApiUrl";
+const apimSasAccessTokenParameter = "managementApiAccessToken";
+const storageSasUrlParameter = "blobStorageUrl";
+const storageConnectionStringParameter = "blobStorageConnectionString";
+
+fs.readFile(configDesignFile, {encoding: 'utf-8'}, function(err,data){
+    if (!err) {
+        var obj = JSON.parse(data);
+        obj[apimServiceParameter] = apimServiceUrlValue;
+        obj[apimSasAccessTokenParameter] = apimSasAccessTokenValue;
+        obj[storageSasUrlParameter] = storageSasUrlValue;
+        fs.writeFile(configDesignFile, JSON.stringify(obj, null, 4), function(errWrite) {
+            if(errWrite) {
+                return console.log(errWrite);
+            }
+        });
+    } else {
+        console.log(err);
+    }
+});
+
+fs.readFile(configPublishFile, {encoding: 'utf-8'}, function(err,data){
+    if (!err) {
+        var obj = JSON.parse(data);
+        obj[apimServiceParameter] = apimServiceUrlValue;
+        obj[apimSasAccessTokenParameter] = apimSasAccessTokenValue;
+        obj[storageConnectionStringParameter] = storageConnectionStringValue;
+        fs.writeFile(configPublishFile, JSON.stringify(obj, null, 4), function(errWrite) {
+            if(errWrite) {
+                return console.log(errWrite);
+            }
+        });
+    } else {
+        console.log(err);
+    }
+});
+
+fs.readFile(configRuntimeFile, {encoding: 'utf-8'}, function(err,data){
+    if (!err) {
+        var obj = JSON.parse(data);
+        obj[apimServiceParameter] = apimServiceUrlValue;
+        fs.writeFile(configRuntimeFile, JSON.stringify(obj, null, 4), function(errWrite) {
+            if(errWrite) {
+                return console.log(errWrite);
+            }
+        });
+    } else {
+        console.log(err);
+    }
+});
