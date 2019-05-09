de6ae4a22f55dca03b9a104f0a33ec43e1c9982d
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2019-05-08 18:24:45 -0700

    Included deleting blobs from the storage into cleanup script

--- scripts/cleanup.bat
+++ scripts/cleanup.bat
@@ -1,4 +1,5 @@
 set service_name="service name"
 set access_token="SharedAccessSignature ..."
+set connection_string="DefaultEndpointsProtocol=..."
 
-node ./cleanup %service_name% %access_token%
+node ./cleanup %service_name% %access_token% %connection_string%
