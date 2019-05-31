cbb8430edf4057d1a9b3cb522517d0db9a044ca2
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2019-05-31 12:57:36 -0700

    Temporary workaround for getting service settings from tenat service

--- src/services/tenantService.ts
+++ src/services/tenantService.ts
@@ -3,22 +3,37 @@ import { TenantSettings } from "../contracts/tenantSettings";
 import { MapiClient } from "./mapiClient";
 import { ServiceDescription } from "../models/service";
 import { HostType } from "../constants";
+import { ISettingsProvider } from "@paperbits/common/configuration";
 
 export class TenantService {
-    constructor(private readonly smapiClient: MapiClient) { }
+    constructor(
+        private readonly smapiClient: MapiClient,
+        private readonly settingsProvider: ISettingsProvider
+    ) { }
 
     private async getServiceDescription(): Promise<ServiceDescription> {
         return await this.smapiClient.get<ServiceDescription>("/");
     }
 
     public async getSettings(): Promise<TenantSettings> {
+        /* TODO: User proper service container */
         const result = await this.smapiClient.get("/tenant/settings");
         return result && result["settings"];
     }
 
     public async getServiceName(): Promise<string> {
-        const settings = await this.getSettings();
-        return settings.ProxyHostName.split(".").shift();
+        /* TODO: User proper service container */
+        const managementApiUrl = await this.settingsProvider.getSetting<string>("managementApiUrl");
+        const regex = /https:\/\/(\w*)\./gm;
+        const match = regex.exec(managementApiUrl);
+
+        if (match && match.length > 0) {
+            const serviceName = match[1];
+            return serviceName;
+        }
+        else {
+            return "";
+        }
     }
 
     public async getServiceSkuName(): Promise<string> {
@@ -38,7 +53,8 @@ export class TenantService {
     }
 
     public async getProxyHostnames(): Promise<string[]> {
-        const settings = await this.getSettings();
-        return [settings.ProxyHostName];
+        /* TODO: User proper service container */
+        const serviceName = await this.getServiceName();
+        return [`${serviceName}.azure-api.net`];
     }
 }
