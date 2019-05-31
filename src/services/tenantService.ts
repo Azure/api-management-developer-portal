35dd25f98b6bef398f81bedcbfbb36f33fd86503
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2019-05-31 12:18:56 -0700

    Cleaned up tenant service.

--- src/services/tenantService.ts
+++ src/services/tenantService.ts
@@ -5,47 +5,10 @@ import { ServiceDescription } from "../models/service";
 import { HostType } from "../constants";
 
 export class TenantService {
-    constructor(
-        private readonly smapiClient: MapiClient
-    ) {
-    }
+    constructor(private readonly smapiClient: MapiClient) { }
 
     private async getServiceDescription(): Promise<ServiceDescription> {
-        // return await this.smapiClient.executeGet<ServiceDescription>("");
-
-        const contract = {
-            id: "",
-            tags: {},
-            eTag: "local",
-            location: "West US",
-            masterLocation: "West US",
-            name: "local",
-            properties: {
-                addresserEmail: "admin@apim.net",
-                createdAtUtc: new Date(),
-                managementApiUrl: "https://aztest.management.azure-api.net",
-                portalUrl: "https://portal.apim.net",
-                provisioningState: "Succeeded",
-                targetProvisioningState: null,
-                publisherEmail: "admin@apim.net",
-                publisherName: "Admin",
-                runtimeUrl: "https://aztest.azure-api.net",
-                scmUrl: "https://scm.apim.net",
-                gatewayUrl: "https://aztest.azure-api.net",
-                staticIps: ["127.0.0.1"],
-                virtualNetworkType: 0,
-                hostnameConfigurations: [],
-                additionalLocations: []
-            },
-            resourceGroupName: "devenv",
-            resourceType: "Microsoft.ApiManagement/service",
-            sku: {
-                name: "Developer",
-                capacity: 1
-            }
-        };
-
-        return new ServiceDescription(<any>contract);
+        return await this.smapiClient.get<ServiceDescription>("/");
     }
 
     public async getSettings(): Promise<TenantSettings> {
@@ -54,17 +17,16 @@ export class TenantService {
     }
 
     public async getServiceName(): Promise<string> {
-        const service = await this.getServiceDescription();
-        return service.name;
+        const settings = await this.getSettings();
+        return settings.ProxyHostName.split(".").shift();
     }
 
     public async getServiceSkuName(): Promise<string> {
-        const service = await this.getServiceDescription();
-        return service.sku.name;
+        return "Developer";
     }
 
     public async getServiceMasterKey(): Promise<string> {
-        return "MasterKey";
+        return undefined;
 
         // const service = await this.getServiceDescription();
 
@@ -76,66 +38,7 @@ export class TenantService {
     }
 
     public async getProxyHostnames(): Promise<string[]> {
-        const service = await this.getServiceDescription();
-
-        if (!service) {
-            throw new Error("Unable to get proxy hostname.");
-        }
-
-        const configs = service.hostnameConfigurations;
-
-        if (configs.length && configs.length > 0) {
-            const hostnames = configs.filter((config) => config.type === HostType.Proxy).map(x => x.hostName);
-
-            if (hostnames.length > 0) {
-                return hostnames;
-            }
-        }
-        return [service.gatewayUrl.split("/")[2]];
-    }
-
-    public async getPortalHostname(): Promise<string> {
-        return "portal.host.name";
-
-        // const service = await this.getServiceDescription();
-
-        // if (service && service.properties) {
-        //     const configs = service.hostnameConfigurations;
-
-        //     if (configs.length && configs.length > 0) {
-        //         const config = configs.find(entry => entry.type === HostType.Portal);
-
-        //         if (config) {
-        //             return config.hostName;
-        //         }
-        //     }
-
-        //     return service.portalUrl && service.portalUrl.split("/")[2];
-        // }
-        // else {
-        //     return null;
-        // }
-    }
-
-    public async getManagementHostname(): Promise<string> {
-        return "management.host.name";
-
-        // const service = await this.getServiceDescription();
-
-        // if (service && service.properties) {
-        //     const configs = service.hostnameConfigurations;
-
-        //     if (configs.length && configs.length > 0) {
-        //         const config = configs.find(entry => entry.type === HostType.Mapi);
-
-        //         if (config) {
-        //             return config.hostName;
-        //         }
-        //     }
-        //     return service.managementApiUrl.split("/")[2];
-        // }
-        // else {
-        //     return null;
-        // }
+        const settings = await this.getSettings();
+        return [settings.ProxyHostName];
     }
 }
