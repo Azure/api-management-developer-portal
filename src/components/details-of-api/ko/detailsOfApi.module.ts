1c57010b6d97d3662de4742cce27c958b70ff25c
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2019-06-03 15:41:22 -0700

    Upgraded paperbits libraries to 0.1.112. Made respective adjustments to match interfaces.

--- src/components/details-of-api/ko/detailsOfApi.module.ts
+++ src/components/details-of-api/ko/detailsOfApi.module.ts
@@ -1,17 +1,11 @@
 import { IInjectorModule, IInjector } from "@paperbits/common/injection";
-import { ViewModelBinder } from "@paperbits/common/widgets";
 import { DetailsOfApiModelBinder } from "../detailsOfApiModelBinder";
 import { DetailsOfApiViewModelBinder } from "./detailsOfApiViewModelBinder";
-import { IModelBinder } from "@paperbits/common/editing";
+
 
 export class DetailsOfApiModule implements IInjectorModule {
     public register(injector: IInjector): void {
-        injector.bind("detailsOfApiModelBinder", DetailsOfApiModelBinder);
-        const modelBinders = injector.resolve<Array<IModelBinder>>("modelBinders");
-        modelBinders.push(injector.resolve("detailsOfApiModelBinder"));
-
-        injector.bind("detailsOfApiViewModelBinder", DetailsOfApiViewModelBinder);
-        const viewModelBinders = injector.resolve<Array<ViewModelBinder<any, any>>>("viewModelBinders");
-        viewModelBinders.push(injector.resolve("detailsOfApiViewModelBinder"));
+        injector.bindToCollection("modelBinders", DetailsOfApiModelBinder);
+        injector.bindToCollection("viewModelBinders", DetailsOfApiViewModelBinder);
     }
 }
