bc9a32d47da3414b013a5d8b698875fb4001d5d5
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2019-06-24 14:30:26 -0700

    Fixes for deploy-default script.

--- scripts/deploy-default.bat
+++ scripts/deploy-default.bat
@@ -4,14 +4,14 @@ cd scripts
 
 set management_endpoint="apim-instance"
 set access_token="SharedAccessSignature integration&..."
-set access_token="https://portalstorage.blob.core.windows.net/?..."
+set storage_sas_url="https://portalstorage.blob.core.windows.net/?..."
 set storage_connection_string="DefaultEndpointsProtocol=https;AccountName=portalstorage;AccountKey=...;EndpointSuffix=core.windows.net"
 set portalUrl="https://portalstorage.../"
 set data_file="./data.json"
 set media_folder="./media"
 
 node ./cleanup %management_endpoint% %access_token% %storage_connection_string%
-node ./configure %management_endpoint% %access_token% %access_token% %storage_connection_string%
+node ./configure %management_endpoint% %access_token% %storage_sas_url% %storage_connection_string%
 node ./generate %management_endpoint% %access_token% %data_file%
 node ./upload %storage_connection_string% %media_folder%
 
