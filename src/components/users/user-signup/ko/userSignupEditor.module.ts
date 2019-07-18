9ba3b1b7eb25387d4c9ad1f51627e6ee19f4d893
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2019-07-18 15:27:22 -0700

    Upgraded paperbits libraries to 0.1.131

--- src/components/users/user-signup/ko/userSignupEditor.module.ts
+++ src/components/users/user-signup/ko/userSignupEditor.module.ts
@@ -1,12 +1,8 @@
 import { IInjectorModule, IInjector } from "@paperbits/common/injection";
-import { IWidgetHandler } from "@paperbits/common/editing";
 import { UserSignupHandlers } from "../userSignupHandlers";
 
 export class UserSignupEditorModule implements IInjectorModule {
-    register(injector: IInjector): void {
-        injector.bindSingleton("userSignupHandlers", UserSignupHandlers);
-
-        const widgetHandlers:Array<IWidgetHandler> = injector.resolve("widgetHandlers");
-        widgetHandlers.push(injector.resolve<UserSignupHandlers>("userSignupHandlers"));
+    public register(injector: IInjector): void {
+        injector.bindToCollection("widgetHandlers", UserSignupHandlers, "userSignupHandlers");
     }
 }
