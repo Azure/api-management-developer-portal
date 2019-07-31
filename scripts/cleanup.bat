b18fbbd4b08cda7aad08c1fad2db68f758c96eca
Mike Budzynski <40186513+mikebudzynski@users.noreply.github.com>
2019-07-30 17:02:07 -0700

    Add comments in the scripts (#127)

--- scripts/cleanup.bat
+++ scripts/cleanup.bat
@@ -1,3 +1,5 @@
+@REM Delete the content of an API Management portal - incl. pages, layouts, configuration, media files, etc.
+
 set management_endpoint="< service name >.management.azure-api.net"
 set access_token="SharedAccessSignature ..."
 set storage_connection_string="DefaultEndpointsProtocol=..."
--- scripts/migrate.bat
+++ scripts/migrate.bat
@@ -1,15 +1,27 @@
+@REM Migrate the content of an API Management portal from one service instance to another - incl. pages, layouts, configuration, media files, etc.
+
 set source_management_endpoint="< source service name >.management.azure-api.net"
 set source_access_token="SharedAccessSignature ..."
 set source_storage_connection_string="DefaultEndpointsProtocol=..."
+
 set target_management_endpoint="< target service name >.management.azure-api.net"
 set target_access_token="SharedAccessSignature ..."
 set target_storage_connection_string="DefaultEndpointsProtocol=..."
+
 set data_file="../dist/data.json"
 set media_folder="../dist/media"
 
-
+@REM Capture the content of the source portal (excl. media)
 node ./capture %source_management_endpoint% %source_access_token% %data_file%
+
+@REM Remove all the content of the target portal (incl. media)
 node ./cleanup %target_management_endpoint% %target_access_token% %target_storage_connection_string%
+
+@REM Upload the content of the source portal (excl. media)
 node ./generate %target_management_endpoint% %target_access_token% %data_file%
+
+@REM Download the media files from the source portal
 node ./download %source_storage_connection_string% %media_folder%
+
+@REM Upload the media files to the target portal
 node ./upload %target_storage_connection_string% %media_folder%
