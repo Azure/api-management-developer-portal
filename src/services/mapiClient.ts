4e0c36eec80ccb3bc4814e52002e3dc68ebc7a2a
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2019-08-01 15:35:55 -0700

    A fix for MAPI client initialization.

--- src/services/mapiClient.ts
+++ src/services/mapiClient.ts
@@ -38,9 +38,7 @@ export class MapiClient {
         if (!this.initializePromise) {
             this.initializePromise = this.initialize();
         }
-        else {
-            return this.initializePromise;
-        }
+        return this.initializePromise;
     }
 
     private async initialize(): Promise<void> {
@@ -157,6 +155,7 @@ export class MapiClient {
 
     private handleResponse<T>(response: HttpResponse<T>, url: string): T {
         let contentType = "";
+
         if (response.headers) {
             const authTokenHeader = response.headers.find(header => header.name === "ocp-apim-sas-token");
 
