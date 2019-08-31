7d205de3b22ddee04f94019d3205c8e98547325c
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2019-08-31 13:49:32 -0700

    Fixed number of accessibility issues. Fixed bug with creating a new page. (#145)

--- src/routing/unsavedChangesRouteGuard.ts
+++ src/routing/unsavedChangesRouteGuard.ts
@@ -13,37 +13,42 @@ export class UnsavedChangesRouteGuard implements RouteGuard {
 
     public canActivate(route: Route): Promise<boolean> {
         return new Promise<boolean>(async (resolve) => {
-            if (this.offlineObjectStorage.hasUnsavedChanges()) {
-                const path = route.path;
+            if (!this.offlineObjectStorage.hasUnsavedChanges()) {
+                resolve(true);
+            }
 
-                if (route.previous && route.previous.path === path) {
-                    const page = await this.pageService.getPageByPermalink(path);
+            const path = route.path;
 
-                    if (page && this.offlineObjectStorage.hasUnsavedChangesAt(page.contentKey)) {
-                        const toast = this.viewManager.addToast("Unsaved changes", `You have unsaved changes. Do you want to save or discard them?`, [
-                            {
-                                title: "Save",
-                                iconClass: "paperbits-check-2",
-                                action: async (): Promise<void> => {
-                                    this.offlineObjectStorage.saveChanges();
-                                    this.viewManager.removeToast(toast);
-                                    resolve(true);
-                                }
-                            },
-                            {
-                                title: "Discard",
-                                iconClass: "paperbits-simple-remove",
-                                action: async (): Promise<void> => {
-                                    this.offlineObjectStorage.discardChanges();
-                                    this.viewManager.removeToast(toast);
-                                    resolve(true);
-                                }
+            const pathNotChanged = route.previous && route.previous.path === path;
+
+            if (pathNotChanged) {
+                const page = await this.pageService.getPageByPermalink(path);
+
+                if (!page || !page.contentKey) {
+                    resolve(true);
+                }
+
+                if (this.offlineObjectStorage.hasUnsavedChangesAt(page.contentKey)) {
+                    const toast = this.viewManager.addToast("Unsaved changes", `You have unsaved changes. Do you want to save or discard them?`, [
+                        {
+                            title: "Save",
+                            iconClass: "paperbits-check-2",
+                            action: async (): Promise<void> => {
+                                await this.offlineObjectStorage.saveChanges();
+                                this.viewManager.removeToast(toast);
+                                resolve(true);
                             }
-                        ]);
-                    }
-                    else {
-                        resolve(true);
-                    }
+                        },
+                        {
+                            title: "Discard",
+                            iconClass: "paperbits-simple-remove",
+                            action: async (): Promise<void> => {
+                                await this.offlineObjectStorage.discardChanges();
+                                this.viewManager.removeToast(toast);
+                                resolve(true);
+                            }
+                        }
+                    ]);
                 }
             }
             else {
