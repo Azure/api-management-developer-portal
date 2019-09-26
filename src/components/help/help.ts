0ff031786bb5830c095f79008c90b7e2794aee27
Igor O <ygrik@hotmail.com>
2019-09-26 15:30:57 -0700

    added help module (#169)
    added help module

---
+++ src/components/help/help.ts
@@ -0,0 +1,10 @@
+﻿import template from "./help.html";
+import { Component } from "@paperbits/common/ko/decorators";
+
+@Component({
+    selector: "help-workshop",
+    template: template,
+    injectable: "helpWorkshop"
+})
+export class HelpWorkshop {
+}
