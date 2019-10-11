9f01794462bba9acc855459c3c149a9353b3acfd
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2019-10-10 17:52:59 -0700

    Small fixes for API list dropdown and sign-in/sign-up forms. (#199)

--- src/components/users/user-signin/ko/runtime/user-signin.ts
+++ src/components/users/user-signin/ko/runtime/user-signin.ts
@@ -1,11 +1,14 @@
 import * as ko from "knockout";
+import * as validation from "knockout.validation";
 import template from "./user-signin.html";
 import { Component, RuntimeComponent, OnMounted } from "@paperbits/common/ko/decorators";
 import { UsersService } from "../../../../../services/usersService";
 import { MapiError } from "../../../../../services/mapiError";
 
 
-@RuntimeComponent({ selector: "user-signin" })
+@RuntimeComponent({
+    selector: "user-signin"
+})
 @Component({
     selector: "user-signin",
     template: template,
@@ -14,20 +17,16 @@ import { MapiError } from "../../../../../services/mapiError";
 export class UserSignin {
     public readonly username: ko.Observable<string>;
     public readonly password: ko.Observable<string>;
-    public readonly canSubmit: ko.Computed<boolean>;
     public readonly errorMessages: ko.ObservableArray<string>;
-    public readonly hasErrors: ko.Observable<boolean>;
+    public readonly hasErrors: ko.Computed<boolean>;
     public readonly working: ko.Observable<boolean>;
 
     constructor(private readonly usersService: UsersService) {
         this.username = ko.observable("");
         this.password = ko.observable("");
         this.errorMessages = ko.observableArray([]);
-        this.hasErrors = ko.observable(false);
+        this.hasErrors = ko.pureComputed(() => this.errorMessages().length > 0);
         this.working = ko.observable(false);
-        this.canSubmit = ko.pureComputed(() => {
-            return !!this.username() && !!this.password() && !this.working();
-        });
     }
 
     @OnMounted()
@@ -38,15 +37,22 @@ export class UserSignin {
             if (userId) {
                 this.navigateToHome();
             }
+
+            validation.init({
+                insertMessages: false,
+                errorElementClass: "is-invalid",
+                decorateInputElement: true
+            });
+
+            this.username.extend(<any>{ required: { message: `Email is required.` }, email: true });
+            this.password.extend(<any>{ required: { message: `Password is required.` }});
         }
         catch (error) {
-            if (error.code === "Unauthorized") {
+            if (error.code === "Unauthorized" || error.code === "ResourceNotFound") {
                 return;
             }
 
-            if (error.code === "ResourceNotFound") {
-                return;
-            }
+            throw error;
         }
     }
 
@@ -60,7 +66,20 @@ export class UserSignin {
     }
 
     public async signin(): Promise<void> {
-        this.hasErrors(false);
+        this.errorMessages([]);
+
+        const result = validation.group({
+            username: this.username,
+            password: this.password
+        });
+
+        const clientErrors = result();
+
+        if (clientErrors.length > 0) {
+            this.errorMessages(clientErrors);
+            return;
+        }
+
         this.working(true);
 
         try {
@@ -71,12 +90,9 @@ export class UserSignin {
             }
             else {
                 this.errorMessages(["Please provide a valid email and password."]);
-                this.hasErrors(true);
             }
         }
         catch (error) {
-            this.hasErrors(true);
-
             if (error instanceof MapiError) {
                 if (error.code === "identity_not_confirmed") {
                     this.errorMessages([`We found an unconfirmed account for the e-mail address ${this.username()}. To complete the creation of your account we need to verify your e-mail address. We’ve sent an e-mail to ${this.username()}. Please follow the instructions inside the e-mail to activate your account. If the e-mail doesn’t arrive within the next few minutes, please check your junk email folder`]);
@@ -85,6 +101,8 @@ export class UserSignin {
                 this.errorMessages([error.message]);
             }
         }
-        this.working(false);
+        finally {
+            this.working(false);
+        }
     }
 }
