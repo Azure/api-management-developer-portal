b5f222e519cdaabb9860680d9e7958397c30d95e
Igor O <ygrik@hotmail.com>
2019-10-28 11:48:24 -0700

    added dev portal setting in constants (#240)

--- src/services/backendService.ts
+++ src/services/backendService.ts
@@ -5,6 +5,7 @@ import { ResetRequest, ResetPassword, ChangePasswordRequest } from "../contracts
 import { IAuthenticator } from "../authentication";
 import { DelegationAction } from "../contracts/tenantSettings";
 import { ISettingsProvider } from "@paperbits/common/configuration/ISettingsProvider";
+import { SettingNames } from "../constants";
 
 export class BackendService {
     private portalUrl;
@@ -133,7 +134,7 @@ export class BackendService {
 
     private async getUrl(path: string): Promise<string> {
         if (!this.portalUrl) {
-            this.portalUrl = await this.settingsProvider.getSetting<string>("devPortalUrl") || "";
+            this.portalUrl = await this.settingsProvider.getSetting<string>(SettingNames.devPortalUrl) || "";
         }
         return `${this.portalUrl}${path}`;
     }
