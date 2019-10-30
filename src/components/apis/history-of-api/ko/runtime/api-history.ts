d988435863bb7596d2c32aa596470351fc5cddf2
RupengLiu <rupengliu123@gmail.com>
2019-10-30 16:37:16 -0700

    Several changes to UI (#264)
    * Several changes to UI
    
    * Update spacing  style
    
    * Identation

--- src/components/apis/history-of-api/ko/runtime/api-history.ts
+++ src/components/apis/history-of-api/ko/runtime/api-history.ts
@@ -19,7 +19,8 @@ export class ApiHistory {
     public api: ko.Observable<Api> = null;
     public apiId: string = null;
     public versionApis: ko.ObservableArray<Api>;
-    public working: ko.Observable<boolean>;
+    public apiWorking: ko.Observable<boolean>;
+    public changeLogWorking: ko.Observable<boolean>;
     public selectedId: ko.Observable<string>;
     public changeLogHasNextPage: ko.Observable<boolean>;
     public changeLogHasPrevPage: ko.Observable<boolean>;
@@ -34,7 +35,8 @@ export class ApiHistory {
     ) {
         this.api = ko.observable();
         this.versionApis = ko.observableArray([]);
-        this.working = ko.observable(false);
+        this.apiWorking = ko.observable(false);
+        this.changeLogWorking = ko.observable(false);
         this.selectedId = ko.observable();
         this.loadApi = this.loadApi.bind(this);
         this.currentPageLog = ko.observableArray([]);
@@ -64,7 +66,7 @@ export class ApiHistory {
         }
 
         try {
-            this.working(true);
+            this.apiWorking(true);
 
             if (apiName) {
                 const api = await this.apiService.getApi(`apis/${apiName}`);
@@ -72,22 +74,22 @@ export class ApiHistory {
                 if (api && api.apiVersionSet && api.apiVersionSet.id) {
                     const apis = await this.apiService.getApisInVersionSet(api.apiVersionSet.id);
                     this.versionApis(apis || []);
-                } 
+                }
                 else {
                     this.versionApis([]);
                 }
                 this.selectedId(api.name);
                 this.api(api);
                 this.apiId = api.id;
-                this.getCurrentPage();
             }
-        } 
-        catch (error) {
-            throw new Error(`Unable to load API history. Error: ${error.message}`);
-        } 
-        finally {
-            this.working(false);
         }
+        catch (error) {
+            throw new Error(`Unable to load API Info. Error: ${error.message}`);
+        }
+        finally {
+            this.apiWorking(false);
+        }
+        this.getCurrentPage();
     }
 
     public dispose(): void {
@@ -105,12 +107,22 @@ export class ApiHistory {
     }
 
     private async getCurrentPage(): Promise<void> {
-        const pageOfLogs = await this.apiService.getApiChangeLog(this.apiId, (this.changeLogPage() - 1) * Constants.defaultPageSize);
-        pageOfLogs.value.map(x => x.properties.updatedDateTime = Utils.formatDateTime(x.properties.updatedDateTime));
-        
-        this.changeLogHasPrevPage(this.changeLogPage() > 1);
-        this.changeLogHasNextPage(!!pageOfLogs.nextLink);
-        this.currentPageLog(pageOfLogs.value);
+        try {
+            this.changeLogWorking(true);
+
+            const pageOfLogs = await this.apiService.getApiChangeLog(this.apiId, (this.changeLogPage() - 1) * Constants.defaultPageSize);
+            pageOfLogs.value.map(x => x.properties.updatedDateTime = Utils.formatDateTime(x.properties.updatedDateTime));
+
+            this.changeLogHasPrevPage(this.changeLogPage() > 1);
+            this.changeLogHasNextPage(!!pageOfLogs.nextLink);
+            this.currentPageLog(pageOfLogs.value);
+        }
+        catch (error) {
+            throw new Error(`Unable to load API history. Error: ${error.message}`);
+        }
+        finally {
+            this.changeLogWorking(false);
+        }
     }
 
     public getReferenceUrl() {
