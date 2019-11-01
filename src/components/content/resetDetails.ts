9136ef65ec47119d8d401c2749a6e3eb74fa6ef1
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2019-11-01 15:24:15 -0700

    Added few basic telemetry events.

--- src/components/content/resetDetails.ts
+++ src/components/content/resetDetails.ts
@@ -3,6 +3,7 @@ import template from "./resetDetails.html";
 import { Component } from "@paperbits/common/ko/decorators";
 import { ViewManager } from "@paperbits/common/ui";
 import { ProvisionService } from "../../services/provisioningService";
+import { Logger } from "@paperbits/common/logging";
 
 
 @Component({
@@ -14,9 +15,11 @@ import { ProvisionService } from "../../services/provisioningService";
 export class ResetDetailsWorkshop {
     public readonly response: ko.Observable<string>;
     public readonly canReset: ko.Computed<boolean>;
+
     constructor (
         private readonly viewManager: ViewManager,
-        private provisioningService: ProvisionService,
+        private readonly provisioningService: ProvisionService,
+        private readonly logger: Logger
     ) {
         this.response = ko.observable("");
         this.canReset = ko.pureComputed(() => this.response().toLocaleLowerCase() === "yes");
@@ -24,10 +27,14 @@ export class ResetDetailsWorkshop {
 
     public async reset(): Promise<void> {
         try {
+            this.logger.traceEvent("Click: Reset website");
+
             this.viewManager.notifySuccess("Website reset", `The website is being reset...`);
 
             await this.provisioningService.cleanup();
             await this.provisioningService.provision();
+
+            this.logger.traceEvent("Success: Website reset");
 
             window.location.reload();
         } 
