091f9bcd40f11a0e0007b3d7d88271979f381402
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2019-12-04 16:58:23 -0800

    Added "enable console" option to the operation details editor. (#352)

--- src/components/operations/operation-details/ko/operationDetailsViewModelBinder.ts
+++ src/components/operations/operation-details/ko/operationDetailsViewModelBinder.ts
@@ -1,20 +1,27 @@
 import { ViewModelBinder } from "@paperbits/common/widgets";
+import { EventManager } from "@paperbits/common/events";
 import { OperationDetailsViewModel } from "./operationDetailsViewModel";
 import { OperationDetailsModel } from "../operationDetailsModel";
 import { Bag } from "@paperbits/common";
 
+
 export class OperationDetailsViewModelBinder implements ViewModelBinder<OperationDetailsModel, OperationDetailsViewModel> {
+    constructor(private readonly eventManager: EventManager) { }
+    
     public async modelToViewModel(model: OperationDetailsModel, viewModel?: OperationDetailsViewModel, bindingContext?: Bag<any>): Promise<OperationDetailsViewModel> {
         if (!viewModel) {
             viewModel = new OperationDetailsViewModel();
         }
+
+        viewModel.config(JSON.stringify({ enableConsole: model.enableConsole }));
 
         viewModel["widgetBinding"] = {
             displayName: "Operation: Details",
             model: model,
             editor: "operation-details-editor",
             applyChanges: async (updatedModel: OperationDetailsModel) => {
-                this.modelToViewModel(updatedModel, viewModel, bindingContext);
+                await this.modelToViewModel(updatedModel, viewModel, bindingContext);
+                this.eventManager.dispatchEvent("onContentUpdate");
             }
         };
 
--- src/components/operations/operation-details/operationDetailsModel.ts
+++ src/components/operations/operation-details/operationDetailsModel.ts
@@ -1,1 +1,10 @@
-export class OperationDetailsModel { }
+export class OperationDetailsModel {
+    /**
+     * Indicates whether "Try" button should appear on the operation details widget.
+     */
+    public enableConsole?: boolean;
+
+    constructor() {
+        this.enableConsole = true;
+    }
+}
