aa1f5e018a7bdbc90069aab56def067237f50902
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2019-12-13 21:38:59 -0800

    Fixed subscription key rendering in test console. (#369)

--- src/models/parameter.ts
+++ src/models/parameter.ts
@@ -1,12 +1,39 @@
 import { ParameterContract } from "../contracts/parameter";
 
 export class Parameter {
+    /**
+     * Parameter name, e.g. api-version.
+     */
     public name: string;
-    public in: string;
+
+    /**
+     * Parameter description.
+     */
     public description: string;
+
+    /**
+     * Parameter placement, e.g. "query", "template", "header", "body".
+     */
+    public in: string;
+
+    /**
+     * Parameter type, e.g. "string", "int64", etc.
+     */
     public type: string;
+
+    /**
+     * Parameter default value, e.g. "2018-06-01-preview".
+     */
     public defaultValue: string;
+
+    /**
+     * Parameter value suggestions, e.g. ["2016-07-07","2016-10-10", "2018-06-01-preview"]
+     */
     public values: string[];
+
+    /**
+     * Indicates if the parameter is required to make a request.
+     */
     public required: boolean;
 
     constructor(placement: string, contract?: ParameterContract) {
