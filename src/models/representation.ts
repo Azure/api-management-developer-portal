fbb84e8fed2393b82a23e8d54aaeeceeeeaf974b
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-01-22 14:54:31 -0800

    Added case for rendering representation samples when no definition specified. (#435)

--- src/models/representation.ts
+++ src/models/representation.ts
@@ -1,17 +1,31 @@
-import { Parameter } from "./parameter";
+import { Utils } from "../utils";
 import { RepresentationContract } from "../contracts/representation";
 
 export class Representation {
     public contentType: string;
-    public sample: string;
+    public example: string;
+    public exampleFormat: string;
     public schemaId: string;
     public typeName: string;
-    public formParameters: Parameter[];
 
     constructor(contract?: RepresentationContract) {
         this.contentType = contract.contentType;
-        this.sample = contract.sample;
+        this.example = contract.sample;
         this.schemaId = contract.schemaId;
         this.typeName = contract.typeName;
+
+        if (contract.sample) {
+            this.example = contract.sample;
+
+            if (contract.contentType.contains("/xml")) {
+                this.example = Utils.formatXml(contract.sample);
+                this.exampleFormat = "xml";
+            }
+
+            if (contract.contentType.contains("/json")) {
+                this.example = Utils.formatJson(contract.sample);
+                this.exampleFormat = "json";
+            }
+        }
     }
 }
