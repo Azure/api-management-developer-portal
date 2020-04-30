d285ff3ae895f58b69e97dd7403c38cd849b73fe
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-04-29 17:21:57 -0700

    Added HTML truncation option to "markdown" binding and updated "description" fields in list components. (#596)

--- src/bindingHandlers/markdown.ts
+++ src/bindingHandlers/markdown.ts
@@ -1,19 +1,48 @@
 import * as ko from "knockout";
 import * as remark from "remark";
 import * as html from "remark-html";
+import * as truncateHtml from "truncate-html";
+
+interface MarkdownConfig {
+    /**
+     * Markdown source.
+     */
+    source: string;
+
+    /**
+     * Maximum length of text before truncation.
+     */
+    truncateAt: number;
+}
 
 
 ko.bindingHandlers["markdown"] = {
-    init: (element: HTMLElement, valueAccessor: () => string): void => {
-        const markdown = ko.unwrap(valueAccessor());
+    init: (element: HTMLElement, valueAccessor: () => string | MarkdownConfig): void => {
+        const config = ko.unwrap(valueAccessor());
         const htmlObservable = ko.observable();
+
+        let markdown: string;
+        let length: number;
+
+        if (!config) {
+            return;
+        }
+
+        if (typeof config === "string") {
+            markdown = config;
+        }
+        else {
+            markdown = config.source;
+            length = config.truncateAt;
+        }
 
         ko.applyBindingsToNode(element, { html: htmlObservable }, null);
 
         remark()
             .use(html)
-            .process(markdown, (err, file) => {
-                htmlObservable(file);
+            .process(markdown, (err: any, html: any) => {
+                html = truncateHtml.default(html, { length: length, reserveLastWord: true });
+                htmlObservable(html);
             });
     }
 };
