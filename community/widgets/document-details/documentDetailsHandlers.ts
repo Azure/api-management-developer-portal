6482cecd2947f5f139842dcb874c59e298de6b5c
Kanchan-Wakchaure <35479753+Kanchan-Wakchaure@users.noreply.github.com>
2020-05-15 17:59:12 -0700

    Kw/widget contribution/document details (#601)

---
+++ community/widgets/document-details/documentDetailsHandlers.ts
@@ -0,0 +1,22 @@
+﻿import { IWidgetOrder, IWidgetHandler } from "@paperbits/common/editing";
+import { DocumentDetailsModel } from "./documentDetailsModel";
+import { widgetName, widgetDisplayName, widgetCategory,defaultFileName } from "./constants";
+
+
+export class DocumentDetailsHandlers implements IWidgetHandler {
+    public async getWidgetOrder(): Promise<IWidgetOrder> {
+        const widgetOrder: IWidgetOrder = {
+            name: widgetName,
+            displayName: widgetDisplayName,
+            category: widgetCategory,
+            iconClass: "paperbits-puzzle-10",
+            createModel: async () => {
+                const model = new DocumentDetailsModel();
+                model.fileName = defaultFileName;
+                return model;
+            }
+        };
+
+        return widgetOrder;
+    }
+}
