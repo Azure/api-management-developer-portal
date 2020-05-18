6533f5a0cc7cefced12018bcfbd39cdb07fbb253
David Freetly <dave.freetly@minnstate.edu>
2020-05-18 14:01:16 -0500

    bash script for Mac users that want to work with the self-hosted developer portal.  There is also a PR for the wiki page 'Self-hosting the portal.md' that references this file. (#618)

---
+++ scripts/generate.sh
@@ -0,0 +1,11 @@
+# Generate and provision default content of an API Management portal - incl. pages, layouts, configuration, media files, etc.
+
+export management_endpoint="< service name >.management.azure-api.net"
+export access_token="SharedAccessSignature ..."
+export storage_connection_string="DefaultEndpointsProtocol=..."
+export data_file="./data.json"
+export media_folder="./media"
+
+# make sure to double quote the $access_token variable so it handles the space correctly
+node ./generate $management_endpoint "$access_token" $data_file
+node ./upload $storage_connection_string $media_folder
