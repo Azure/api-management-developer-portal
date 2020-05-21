22fa957e98047e7a42c7db04f523490749d7b810
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-05-21 13:23:06 -0700

    Optimized build scripts. Upgraded dependencies. (#631)

--- webpack.publisher.js
+++ webpack.publisher.js
@@ -7,15 +7,13 @@ const runtimeConfig = require("./webpack.runtime");
 
 const publisherConfig = {
     mode: "development",
-    devtool: "inline-source-map",
     target: "node",
     node: {
         __dirname: false,
         __filename: false,
     },
     entry: {
-        "index": ["./src/startup.publish.ts"],
-        "assets/styles/theme": [`./src/themes/website/styles/styles.scss`]
+        "index": ["./src/startup.publish.ts"]
     },
     output: {
         filename: "./[name].js",
@@ -40,11 +38,21 @@ const publisherConfig = {
             },
             {
                 test: /\.html$/,
-                loader: "html-loader?exportAsEs6Default"
+                loader: "html-loader",
+                options: {
+                    esModule: true,
+                    minimize: {
+                        removeComments: false,
+                        collapseWhitespace: false
+                    }
+                }
             },
             {
                 test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-                loader: "url-loader?limit=100000"
+                loader: "url-loader",
+                options: {
+                    limit: 10000
+                }
             },
             {
                 test: /\.liquid$/,
@@ -55,18 +63,15 @@ const publisherConfig = {
     plugins: [
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({ filename: "[name].css", chunkFilename: "[id].css" }),
-        new CopyWebpackPlugin([
-            { from: `./src/config.publish.json`, to: `config.json` },
-            { from: `./src/config.runtime.json`, to: `assets/config.json` },
-            { from: `./src/themes/website/styles/fonts`, to: "assets/styles/fonts" },
-            { from: `./js/HipObject.js`, to: "assets/scripts/js" }
-        ])
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: `./src/config.publish.json`, to: `config.json` }
+            ]
+        })
     ],
     resolve: {
         extensions: [".ts", ".tsx", ".js", ".jsx", ".html", ".scss"]
     }
 };
 
-runtimeConfig.output.path = path.resolve(__dirname, "dist/publisher");
-
-module.exports = [runtimeConfig, publisherConfig];
+module.exports = [publisherConfig, runtimeConfig(false)];
