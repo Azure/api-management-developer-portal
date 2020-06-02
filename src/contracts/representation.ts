13ed2dc0bc78de604e3211b6c125df5e46e09cb1
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-06-02 13:11:15 -0700

    Taking into account generated representation samples. (#672)

--- src/contracts/representation.ts
+++ src/contracts/representation.ts
@@ -1,12 +1,36 @@
-import { ParameterContract } from './parameter';
+import { ParameterContract } from "./parameter";
 
 /**
- * Model of API operation payload representation
+ * Contract for API operation representation.
  */
 export interface RepresentationContract {
+    /**
+     * Content type, e.g. application/json.
+     */
     contentType: string;
+
+    /**
+     * User created sample.
+     */
     sample?: string;
+
+    /**
+     * Sample generated during API import.
+     */
+    generatedSample?: string;
+
+    /**
+     * Identifier of the schema the representation belongs to.
+     */
     schemaId?: string;
+
+    /**
+     * Name of the type describing representation.
+     */
     typeName?: string;
+
+    /**
+     * Description of the form parameters, if the representation has form-like payload.
+     */
     formParameters?: ParameterContract[];
 }
