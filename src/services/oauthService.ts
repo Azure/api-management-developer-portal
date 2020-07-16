8d1eac46b3a1e96e1f69458cf54d89ae3f7dff5b
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-07-16 15:40:46 -0700

    Added basic support for OpenID Connect. (#756)

--- src/services/oauthService.ts
+++ src/services/oauthService.ts
@@ -1,18 +1,50 @@
+import { OpenIdConnectMetadata } from "./../contracts/openIdConnectMetadata";
+
 import * as ClientOAuth2 from "client-oauth2";
+import { HttpClient } from "@paperbits/common/http";
 import { GrantTypes } from "./../constants";
 import { MapiClient } from "./mapiClient";
 import { AuthorizationServerContract } from "../contracts/authorizationServer";
 import { AuthorizationServer } from "../models/authorizationServer";
 import { PageContract } from "../contracts/page";
+import { OpenIdConnectProviderContract } from "../contracts/openIdConnectProvider";
+import { OpenIdConnectProvider } from "./../models/openIdConnectProvider";
 
 export class OAuthService {
-    constructor(private readonly mapiClient: MapiClient) { }
+    constructor(
+        private readonly mapiClient: MapiClient,
+        private readonly httpClient: HttpClient
+    ) { }
+
+    public async getOpenIdConnectProviders(): Promise<OpenIdConnectProvider[]> {
+        try {
+            const pageOfAuthservers = await this.mapiClient.get<PageContract<OpenIdConnectProviderContract>>("/openidConnectProviders");
+            return pageOfAuthservers.value.map(authServer => new OpenIdConnectProvider(authServer));
+        }
+        catch (error) {
+            throw new Error(`Unable to fetch configured authorization servers.`);
+        }
+    }
 
     public async getOAuthServers(): Promise<AuthorizationServer[]> {
         try {
-            const pageOfAuthservers = await this.mapiClient.get<PageContract<AuthorizationServerContract>>("/authorizationServers");
+            const authorizationServers = [];
+            const pageOfOAuthServers = await this.mapiClient.get<PageContract<AuthorizationServerContract>>("/authorizationServers");
+            const oauthServers = pageOfOAuthServers.value.map(authServer => new AuthorizationServer(authServer));
+            authorizationServers.push(...oauthServers);
 
-            return pageOfAuthservers.value.map(authServer => new AuthorizationServer(authServer));
+            const pageOfOicdServers = await this.mapiClient.get<PageContract<OpenIdConnectProviderContract>>("/openidConnectProviders");
+            const oicdServers = pageOfOicdServers.value.map(authServer => new OpenIdConnectProvider(authServer));
+
+            for (const provider of oicdServers) {
+                const authServer = await this.discoverOAuthServer(provider.metadataEndpoint);
+                authServer.clientId = provider.clientId;
+                authServer.displayName = provider.displayName;
+                authServer.description = provider.description;
+                authorizationServers.push(authServer);
+            }
+
+            return authorizationServers;
         }
         catch (error) {
             throw new Error(`Unable to fetch configured authorization servers.`);
@@ -72,7 +104,7 @@ export class OAuthService {
     }
 
     public async authenticateCode(authorizationServer: AuthorizationServer): Promise<string> {
-        const redirectUri = `https://${location.hostname}/signin-oauth/code/callback/${authorizationServer.id}`;
+        const redirectUri = `https://${location.hostname}/signin-oauth/code/callback/${authorizationServer.name}`;
 
         const oauthClient = new ClientOAuth2({
             clientId: authorizationServer.clientId,
@@ -100,7 +132,7 @@ export class OAuthService {
     }
 
     public async authenticateClientCredentials(authorizationServer: AuthorizationServer): Promise<string> {
-        const uri = `https://${location.hostname}/signin-oauth/credentials/${authorizationServer.id}`;
+        const uri = `https://${location.hostname}/signin-oauth/credentials/${authorizationServer.name}`;
 
         return new Promise<string>((resolve, reject) => {
             window.open(uri, "_blank", "width=400,height=500");
@@ -117,5 +149,23 @@ export class OAuthService {
 
             window.addEventListener("message", receiveMessage, false);
         });
+    }
+
+    public async discoverOAuthServer(metadataEndpoint: string): Promise<AuthorizationServer> {
+        const response = await this.httpClient.send<OpenIdConnectMetadata>({ url: metadataEndpoint });
+        const metadata = response.toObject();
+
+        const server = new AuthorizationServer();
+        server.authorizationEndpoint = metadata.authorization_endpoint;
+        server.tokenEndpoint = metadata.token_endpoint;
+        server.scopes = metadata.scopes_supported || ["openid"];
+
+        const supportedGrantTypes = Object.values(GrantTypes).map(x => x.toString());
+        
+        server.grantTypes = metadata.grant_types_supported
+            ? metadata.grant_types_supported.filter(grantType => supportedGrantTypes.includes(grantType))
+            : [GrantTypes.authorizationCode];
+
+        return server;
     }
 }
