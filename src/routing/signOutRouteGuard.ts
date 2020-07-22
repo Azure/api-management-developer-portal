66e103319c640c9e89129c9a869e4d8cf9865cc0
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-07-22 16:07:26 -0700

    Rolledback signout guard.

--- src/routing/signOutRouteGuard.ts
+++ src/routing/signOutRouteGuard.ts
@@ -1,37 +1,16 @@
 import { RouteGuard, Route } from "@paperbits/common/routing";
 import { IAuthenticator } from "../authentication";
-import { hashSignOut } from "../constants";
-import { MapiClient } from "../services/mapiClient";
-import { Identity } from "../contracts/identity";
+import { pageUrlHome, hashSignOut } from "../constants";
 
 export class SignOutRouteGuard implements RouteGuard {
-    constructor(
-        private readonly mapiClient: MapiClient,
-        private readonly authenticator: IAuthenticator
-    ) { }
+
+    constructor(private readonly authenticator: IAuthenticator) { }
 
     public async canActivate(route: Route): Promise<boolean> {
-        if (route.hash !== hashSignOut) {
-            return true;
+        if (route.hash === hashSignOut) {
+            this.authenticator.clearAccessToken();
+            location.assign(pageUrlHome);
         }
-
-        const token = await this.authenticator.getAccessToken();
-        let userId = "";
-
-        if (token) {
-            try {
-                const identity = await this.mapiClient.get<Identity>("/identity");
-
-                if (identity) {
-                    userId = `?userId=${identity.id}`;
-                }
-            }
-            catch (error) {
-                return true;
-            }
-        }
-
-        this.authenticator.clearAccessToken();
-        location.assign("/signout" + userId);
+        return true;
     }
 }
