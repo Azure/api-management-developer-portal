cbea0ba6391caa0dd001a16b5382a5eb0d1bc4e1
Gordon Byers <gordonbyers@gmail.com>
2020-07-31 17:35:37 +0100

    Scripts: move blob container name to a variable, rather than hardcoding to 'content' (#802)

--- scripts/generate.bat
+++ scripts/generate.bat
@@ -3,8 +3,9 @@
 set management_endpoint="< service name >.management.azure-api.net"
 set access_token="SharedAccessSignature ..."
 set storage_connection_string="DefaultEndpointsProtocol=..."
+set container="content"
 set data_file="./data.json"
 set media_folder="./media"
 
 node ./generate %management_endpoint% %access_token% %data_file%
-node ./upload %storage_connection_string% %media_folder%
+node ./upload %storage_connection_string% %media_folder% %container%
