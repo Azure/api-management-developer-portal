7b0ab9354479191e9cac536cec20e22c59194299
Gordon Byers <gordonbyers@gmail.com>
2020-07-31 12:16:21 +0100

    put container in a variable, rather than hardcoding to 'content'

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,9 +3,11 @@
 export management_endpoint="< service name >.management.azure-api.net"
 export access_token="SharedAccessSignature ..."
 export storage_connection_string="DefaultEndpointsProtocol=..."
+export container="content"
 export data_file="./data.json"
 export media_folder="./media"
 
+
 # make sure to double quote the $access_token variable so it handles the space correctly
 node ./generate $management_endpoint "$access_token" $data_file
-node ./upload $storage_connection_string $media_folder
+node ./upload $storage_connection_string $media_folder $container
