0341978a126440a378db110b3f1927207ed3278b
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-08-24 14:11:23 -0700

    Fixed several designer issues

--- src/startup.runtime.ts
+++ src/startup.runtime.ts
@@ -1,17 +1,9 @@
 import { InversifyInjector } from "@paperbits/common/injection";
 import { ApimRuntimeModule } from "./apim.runtime.module";
-import { HistoryRouteHandler, LocationRouteHandler } from "@paperbits/common/routing";
 
 
 const injector = new InversifyInjector();
 injector.bindModule(new ApimRuntimeModule());
-
-if (location.href.includes("designtime=true")) {
-    injector.bindToCollection("autostart", HistoryRouteHandler);
-}
-else {
-    injector.bindToCollection("autostart", LocationRouteHandler);
-}
 
 document.addEventListener("DOMContentLoaded", () => {
     injector.resolve("autostart");
