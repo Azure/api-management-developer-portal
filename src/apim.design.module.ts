3c50d971de0008a9affeb6d08ae6761f78f8c24f
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-08-24 15:30:45 -0700

    Fixed several designer issues (#844)

--- src/apim.design.module.ts
+++ src/apim.design.module.ts
@@ -57,6 +57,7 @@ import { StaticRoleService } from "./services/roleService";
 import { ProvisionService } from "./services/provisioningService";
 import { PolicyService } from "./services/policyService";
 import { OAuthService } from "./services/oauthService";
+import { HistoryRouteHandler } from "@paperbits/common/routing";
 
 
 export class ApimDesignModule implements IInjectorModule {
@@ -121,5 +122,6 @@ export class ApimDesignModule implements IInjectorModule {
         injector.bindToCollection("routeGuards", UnsavedChangesRouteGuard);
         injector.bindInstance("reservedPermalinks", Constants.reservedPermalinks);
         injector.bindSingleton("oauthService", OAuthService);
+        injector.bindToCollection("autostart", HistoryRouteHandler);
     }
 }
