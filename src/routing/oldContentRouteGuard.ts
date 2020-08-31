d3f78def98fc1c0bb404a4016e3235b4c62c40f7
Igor O <igo@microsoft.com>
2020-08-31 11:39:01 -0700

    changed search by tag on selector

--- src/routing/oldContentRouteGuard.ts
+++ src/routing/oldContentRouteGuard.ts
@@ -16,8 +16,8 @@ export class OldContentRouteGuard implements RouteGuard {
         const main = window.document.getElementsByTagName("iframe");
         if (main.length > 0) {
             const iframe = main[0];
-            const deprecatedNavItems = iframe.contentDocument.body.getElementsByTagName("navbar");
-            if (deprecatedNavItems.length > 0) {
+            const deprecatedNavbar = iframe.contentDocument.body.querySelector("nav .navbar-brand");
+            if (deprecatedNavbar) {
                 this.toast = this.viewManager.addToast("Deprecated content detected", `Your developer portal's content is based off the pre-production version of default content. <a href="https://aka.ms/apimdocs/portal#preview-to-ga" target="_blank">Learn about the problems it may cause and how to switch to the production version of content</a>.`);
                 setTimeout(() => {
                     this.viewManager.removeToast(this.toast);
