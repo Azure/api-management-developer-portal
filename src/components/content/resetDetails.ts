9422ab83e97eea67ba89dd9a53ee7f20d633efa1
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-11-30 17:03:52 -0800

    Renamed AppInsightsLogger. Added local storage cleanup to website reset view. (#1066)

--- src/components/content/resetDetails.ts
+++ src/components/content/resetDetails.ts
@@ -29,7 +29,8 @@ export class ResetDetailsWorkshop {
     public async reset(): Promise<void> {
         try {
             this.logger.trackEvent("Click: Reset website");
-
+            
+            localStorage.clear();
             this.offlineObjectStorage.discardChanges();
             this.viewManager.clearJourney();
             this.viewManager.hideToolboxes();
