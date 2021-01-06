a89d838cb8468f3c88a038417dad72dc06d5513b
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2021-01-05 16:56:00 -0800

    Fixed token setting issue. (#1108)

--- src/services/oauthService.ts
+++ src/services/oauthService.ts
@@ -49,7 +49,7 @@ export class OAuthService {
             return authorizationServers;
         }
         catch (error) {
-            throw new Error(`Unable to fetch configured authorization servers.`);
+            throw new Error(`Unable to fetch configured authorization servers. ${error.stack}`);
         }
     }
 
