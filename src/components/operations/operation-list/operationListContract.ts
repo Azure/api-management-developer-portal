4059cf3b0204eac60fd3f12c96b5ec27f054909d
Igor O <igo@microsoft.com>
2021-01-12 19:05:27 -0800

    Added toggle to manage operation name wrap Added toggle to manage showing operation urlTemplate or name

--- src/components/operations/operation-list/operationListContract.ts
+++ src/components/operations/operation-list/operationListContract.ts
@@ -11,6 +11,21 @@ export interface OperationListContract extends Contract {
     allowSelection: boolean;
 
     /**
+     * Indicated that an operations name should wraps to new line if it's too long.
+     */
+    wrapText: boolean;
+
+    /**
+     * Allow switching between URL paths and operation names
+     */
+    showToggleUrlPath: boolean;
+    
+    /**
+     * Show URL paths instead of operation names by default.
+     */
+    defaultShowUrlPath: boolean;
+
+    /**
      * Default GroupByTag to enabled.
      */
     defaultGroupByTagToEnabled?: boolean;
