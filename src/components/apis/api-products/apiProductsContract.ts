12cd7268b861e9e127679e0888cdbbb68b836930
Igor O <ygrik@hotmail.com>
2021-01-22 13:51:40 -0800

    Added api products widgets (grid and tiles) (#1126)
    * Added API products widgets (grid and tiles)
    
    * added style class for tiles
    
    closes #1072

---
+++ src/components/apis/api-products/apiProductsContract.ts
@@ -0,0 +1,17 @@
+import { Contract } from "@paperbits/common";
+import { HyperlinkContract } from "@paperbits/common/editing";
+
+/**
+ * Product API list widget configuration.
+ */
+export interface ApiProductsContract extends Contract {
+    /**
+     * List layout. "list" or "tiles"
+     */
+    itemStyleView?: string;
+
+    /**
+     * Link to a page that contains operation details.
+     */
+    detailsPageHyperlink?: HyperlinkContract;
+}
