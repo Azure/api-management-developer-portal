632c199fb2385aad3e86471b1430c6c389e6e5ca
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2021-06-17 17:44:28 -0700

    Leveraged B2C signout. Moved AAD/B2C configuration publishing into config.json. Added option to override reply URL(s). (#1346)

--- src/components/users/signin-social/signinSocialModel.ts
+++ src/components/users/signin-social/signinSocialModel.ts
@@ -17,9 +17,19 @@ export class SigninSocialModel {
     public aadLabel?: string;
 
     /**
+     * AAD reply URL, e.g. `/signin`.
+     */
+     public aadReplyUrl?: string;
+
+    /**
      * Label on AAD B2C button.
      */
     public aadB2CLabel?: string;
+
+    /**
+     * AAD B2C reply URL, e.g. `/signin`.
+     */
+    public aadB2CReplyUrl?: string;
 
     constructor() {
         this.styles = { appearance: "components/button/default" };
