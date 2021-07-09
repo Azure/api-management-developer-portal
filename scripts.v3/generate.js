a3a9a9d698308d1c65847e774c1219aee389ab53
Erik Mogensen <ErikMogensen@users.noreply.github.com>
2021-07-09 22:18:41 +0200

    Added support for setting folders and timestamp (#1365)
    Co-authored-by: Erik Mogensen <erik.mogensen@users.noreply.github.com>

--- scripts.v3/generate.js
+++ scripts.v3/generate.js
@@ -10,60 +10,78 @@
  * 
  * 3) Run this script with a valid combination of arguments:
  *    node ./cleanup ^
- *    --sourceSubscriptionId "< your subscription ID >" ^
- *    --sourceResourceGroupName "< your resource group name >" ^
- *    --sourceServiceName "< your service name >" ^
- *    --destSubscriptionId "< your subscription ID >" ^
- *    --destResourceGroupName "< your resource group name >" ^
- *    --destServiceName "< your service name >"
+ *   --subscriptionId < your subscription ID > ^
+ *   --resourceGroupName < your resource group name > ^
+ *   --serviceName < your service name >
  */
 
-const { ImporterExporter } = require("./utils");
-
-const yargs = require('yargs')
-    .example(`node ./generate ^ \r
-    --subscriptionId "< your subscription ID >" ^ \r
-    --resourceGroupName "< your resource group name >" ^ \r
-    --serviceName "< your service name >"\n`)
-    .option('subscriptionId', {
-        type: 'string',
-        description: 'Azure subscription ID.',
-        demandOption: true
-    })
-    .option('resourceGroupName', {
-        type: 'string',
-        description: 'Azure resource group name.',
-        demandOption: true
-    })
-    .option('serviceName', {
-        type: 'string',
-        description: 'API Management service name.',
-        demandOption: true
-    })
-    .help()
-    .argv;
-
-async function generate() {
-    const importerExporter = new ImporterExporter(
-        yargs.subscriptionId,
-        yargs.resourceGroupName,
-        yargs.serviceName
-    );
-
-    await importerExporter.import();
-}
-
-generate()
-    .then(() => {
-        console.log("DONE");
-        process.exit(0);
-    })
-    .catch(error => {
-        console.error(error.message);
-        process.exit(1);
-    });
-
-
-module.exports = {
-    generate
-}
+ const path = require("path");
+ const { ImporterExporter } = require("./utils");
+ 
+ const yargs = require('yargs')
+     .example(`node ./generate ^ \r
+     --subscriptionId "< your subscription ID >" ^ \r
+     --resourceGroupName "< your resource group name >" ^ \r
+     --serviceName "< your service name >"\n`)
+     .option('subscriptionId', {
+         type: 'string',
+         description: 'Azure subscription ID.',
+         demandOption: true
+     })
+     .option('resourceGroupName', {
+         type: 'string',
+         description: 'Azure resource group name.',
+         demandOption: true
+     })
+     .option('serviceName', {
+         type: 'string',
+         description: 'API Management service name.',
+         demandOption: true
+     })
+     .option('folder', {
+         type: 'string',
+         default: '../dist/snapshot',
+         description: 'The path to the folder which contains the content to be uploaded to the portal',
+         example: '../dist/snapshot',
+         demandOption: false
+     })
+     .help()
+     .argv;
+ 
+ async function generate() {
+ 
+     // make the folder path understandable if running in Windows
+     const folder = yargs.folder.split("\\").join("/");
+ 
+     // get the absolute path
+     var absoluteFolder = path.resolve(folder);
+      console.log(`Going to upload the content in ${absoluteFolder}.`);
+ 
+     const importerExporter = new ImporterExporter(
+         yargs.subscriptionId,
+         yargs.resourceGroupName,
+         yargs.serviceName,
+         null,
+         null,
+         null,
+         absoluteFolder
+     );
+ 
+     await importerExporter.import();
+ }
+ 
+ generate()
+     .then(() => {
+         console.log("DONE");
+         process.exit(0);
+     })
+     .catch(error => {
+         console.error(error.message);
+         process.exit(1);
+     });
+ 
+ 
+ module.exports = {
+     generate
+ }
+ 
