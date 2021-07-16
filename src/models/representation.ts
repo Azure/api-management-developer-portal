50bed71ff90d05805e8275311b693da4eae7b350
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2021-07-15 18:47:01 -0700

    Added basic mulitpart form data support to API documentation. (#1381)

--- src/models/representation.ts
+++ src/models/representation.ts
@@ -1,14 +1,15 @@
 import { Utils } from "../utils";
 import { RepresentationContract } from "../contracts/representation";
 import { Parameter } from "./parameter";
+import { KnownMimeTypes } from "./knownMimeTypes";
 
 export class Representation {
-    public contentType: string;
-    public example: string;
-    public exampleFormat: string;
-    public schemaId: string;
-    public typeName: string;
-    public formParameters: Parameter[];
+    public readonly contentType: string;
+    public readonly example: string;
+    public readonly exampleFormat: string;
+    public readonly schemaId: string;
+    public readonly typeName: string;
+    public readonly formParameters: Parameter[];
 
     constructor(contract?: RepresentationContract) {
         this.contentType = contract.contentType;
@@ -16,10 +17,9 @@ export class Representation {
         this.schemaId = contract.schemaId;
         this.typeName = contract.typeName;
 
-        if (this.contentType === "multipart/form-data") {
-            this.typeName = "formData";
-            if (contract.formParameters?.length > 0){
-                this.formParameters = contract.formParameters.map(parameterContract => new Parameter("body", parameterContract)); 
+        if (this.contentType === KnownMimeTypes.FormData) {
+            if (contract.formParameters?.length > 0) {
+                this.formParameters = contract.formParameters.map(parameterContract => new Parameter("body", parameterContract));
             }
         }
 
