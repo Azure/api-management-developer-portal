620d7e4531d9bf075d6dc4ddff3264ad4c4a7fca
Travis Whipps <88871562+traviswhipps@users.noreply.github.com>
2021-08-20 16:54:34 +0930

    Added functionality to show legal text by default on product page load (#1414)

--- src/components/products/product-subscribe/ko/productSubscribeEditor.module.ts
+++ src/components/products/product-subscribe/ko/productSubscribeEditor.module.ts
@@ -1,8 +1,11 @@
 import { IInjectorModule, IInjector } from "@paperbits/common/injection";
 import { ProductSubscribeHandlers } from "../productSubscribeHandlers";
+import { ProductSubscribeEditor } from "./productSubscribeEditor";
+
 
 export class ProductSubscribeEditorModule implements IInjectorModule {
     public register(injector: IInjector): void {
+		injector.bind("productSubscribeEditor", ProductSubscribeEditor);
         injector.bindToCollection("widgetHandlers", ProductSubscribeHandlers, "productSubscribeHandlers");
     }
 }
--- src/components/products/product-subscribe/productSubscribeContract.ts
+++ src/components/products/product-subscribe/productSubscribeContract.ts
@@ -1,3 +1,10 @@
 import { Contract } from "@paperbits/common";
 
-export interface ProductSubscribeContract extends Contract { }
+export interface ProductSubscribeContract extends Contract {
+	
+	 /**
+     * Controls whether legal text is displayed by default or not.
+     */
+    showTermsByDefault: boolean;
+
+}
