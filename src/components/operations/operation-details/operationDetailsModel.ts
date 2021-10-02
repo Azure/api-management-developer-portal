a63f9d3344d78890e464c56860010474b86fe2ff
Igor O <ygrik@hotmail.com>
2021-10-01 17:09:15 -0700

    Refactored auth servers load to allow users to use more items (#1471)
    refactored auth servers load to allow users to use more items

--- src/components/operations/operation-details/operationDetailsModel.ts
+++ src/components/operations/operation-details/operationDetailsModel.ts
@@ -1,5 +1,3 @@
-import { AuthorizationServer } from "./../../../models/authorizationServer";
-
 export class OperationDetailsModel {
     /**
      * Indicates whether "Try" button should appear on the operation details widget.
@@ -10,11 +8,6 @@ export class OperationDetailsModel {
      * Defines how schema gets presented in operation details by default, e.g. "table" or "raw".
      */
     public defaultSchemaView?: string;
-
-    /**
-     * External OAuth servers associated with API of this operation.
-     */
-    public authorizationServers: AuthorizationServer[];
 
     /**
      * Indicates whether operation details should appear in the visible area (for example if API details is too long).
