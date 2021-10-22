be73f8b569a03cebfab9a635501c5ab2ac5b3545
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2021-10-21 17:27:45 -0700

    Upgraded dependencies. Improved card set snippet. (#1493)

--- src/services/templatingService.ts
+++ src/services/templatingService.ts
@@ -3,7 +3,7 @@ import { Liquid } from "liquidjs";
 export class TemplatingService {
     public static async render(template: string, model: Object): Promise<string> {
         const engine = new Liquid();
-        const result = await engine.parseAndRender(template, model, null);
+        const result = await engine.parseAndRender(template, model);
 
         return result;
     }
