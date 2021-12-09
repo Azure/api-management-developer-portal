8c803d0f51a67f5073aa6a5bcd0a7b6c588bfc29
Elena Shorohova <92329687+jsorohova@users.noreply.github.com>
2021-12-09 03:32:23 +0200

    Remove item icon and OAuth sign-in and sign-up message (#1553)

--- src/components/users/signup-social/ko/signupSocialViewModelBinder.ts
+++ src/components/users/signup-social/ko/signupSocialViewModelBinder.ts
@@ -1,10 +1,17 @@
 import { Bag } from "@paperbits/common";
 import { ComponentFlow } from "@paperbits/common/editing";
 import { ViewModelBinder } from "@paperbits/common/widgets";
+import { IdentityService } from "../../../../services/identityService";
 import { SignupSocialModel } from "../signupSocialModel";
 import { SignupSocialViewModel } from "./signupSocialViewModel";
+import { ISettingsProvider } from "@paperbits/common/configuration";
 
 export class SignupSocialViewModelBinder implements ViewModelBinder<SignupSocialModel, SignupSocialViewModel> {
+    constructor(
+        private readonly identityService: IdentityService,
+        private readonly settingsProvider: ISettingsProvider
+    ) { }
+
     public async modelToViewModel(model: SignupSocialModel, viewModel?: SignupSocialViewModel, bindingContext?: Bag<any>): Promise<SignupSocialViewModel> {
         if (!viewModel) {
             viewModel = new SignupSocialViewModel();
@@ -16,6 +23,17 @@ export class SignupSocialViewModelBinder implements ViewModelBinder<SignupSocial
             flow: ComponentFlow.Block,
             draggable: true
         };
+
+        const identityProviders = await this.identityService.getIdentityProviders();
+
+        const identityProvider = identityProviders.find(x => x.type === "aad" || x.type === "aadB2C");
+
+        if (identityProvider) {
+            viewModel.identityProvider(true);
+        }
+
+        const settings = await this.settingsProvider.getSettings();
+        viewModel.mode(settings["environment"]);
 
         return viewModel;
     }
