98f8a9e85b0fca062525aa57429c847c11059d1b
Cristina Malin <malincrist@microsoft.com>
2022-01-19 15:27:58 +0100

    Displayed OAuth server configuration

--- src/apim.runtime.module.ts
+++ src/apim.runtime.module.ts
@@ -74,6 +74,7 @@ import { UsersService } from "./services/usersService";
 import { ApimSettingsProvider } from "./configuration/apimSettingsProvider";
 import { AccessTokenRefrsher } from "./authentication/accessTokenRefresher";
 import { Pagination } from "./components/pagination/pagination";
+import { OauthServerConfiguration } from "./components/operations/operation-details/ko/runtime/oauth-server-configuration";
 
 export class ApimRuntimeModule implements IInjectorModule {
     public register(injector: IInjector): void {
@@ -140,5 +141,6 @@ export class ApimRuntimeModule implements IInjectorModule {
         injector.bind("tagInput", TagInput);
         injector.bindToCollection("autostart", AccessTokenRefrsher);
         injector.bind("pagination", Pagination);
+        injector.bind("oauthServerConfiguration", OauthServerConfiguration);
     }
 }
---
+++ src/components/operations/operation-details/ko/runtime/oauth-server-configuration.ts
@@ -0,0 +1,21 @@
+import * as ko from "knockout";
+import template from "./oauth-server-configuration.html";
+import { Component, Param } from "@paperbits/common/ko/decorators";
+import { AuthorizationServer } from "../../../../../models/authorizationServer";
+
+@Component({
+    selector: "oauth-server-configuration",
+    template: template,
+})
+
+export class OauthServerConfiguration {
+
+    public displayedGrantFlows: ko.Observable<string[]>;
+
+    constructor() {
+        this.authorizationServer = ko.observable();
+        this.displayedGrantFlows = ko.observable();
+    }
+    @Param()
+    public authorizationServer: ko.Observable<AuthorizationServer>;
+}
