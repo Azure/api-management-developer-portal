fbd14d263740335037080be23bc9efb80b355857
Javier Borrego Fernandez <javierbo@microsoft.com>
2022-02-03 17:30:27 -0600

    Fixed node generation for graphql interface types and added monaco path for webpack.runtime

--- webpack.runtime.js
+++ webpack.runtime.js
@@ -62,6 +62,7 @@ const runtimeConfig = {
         new CopyWebpackPlugin({
             patterns: [
                 { from: `./src/themes/website/styles/fonts`, to: "styles/fonts" },
+                { from: `node_modules/monaco-editor/min/vs`, to: `assets/monaco-editor/vs` },
                 { from: `./src/themes/website/assets` }
             ]
         }),
