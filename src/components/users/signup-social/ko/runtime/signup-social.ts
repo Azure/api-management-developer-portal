7809300717c193f7ed5e31f067c56178f908cabf
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2022-02-11 13:44:12 -0800

    Fixed issue with populating email from JWT token. (#1651)

--- src/components/users/signup-social/ko/runtime/signup-social.ts
+++ src/components/users/signup-social/ko/runtime/signup-social.ts
@@ -63,7 +63,7 @@ export class SignupSocial {
 
         this.firstName(jwtToken.given_name);
         this.lastName(jwtToken.family_name);
-        this.email(jwtToken.email);
+        this.email(jwtToken.email || jwtToken.emails?.[0]);
     }
 
     /**
