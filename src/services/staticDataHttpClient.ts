0c764ce1d1a22cdec1a4845f80c0c1629e31b668
malincrist <92857141+malincrist@users.noreply.github.com>
2022-02-22 12:44:58 +0100

    Create pipeline that will automatically check for Accessibility Issues (#1584)
    * Create mainCI.yml
    
    Work in progress, don't merge
    
    * Update mainCI.yml
    
    * Update mainCI.yml
    
    * Update mainCI.yml
    
    * Update mainCI.yml
    
    * Update mainCI.yml
    
    * added script for build website
    
    * Update mainCI.yml
    
    * Update mainCI.yml
    
    * Update mainCI.yml
    
    * Update mainCI.yml
    
    * Update mainCI.yml
    
    * Update mainCI.yml
    
    * Update mainCI.yml
    
    * Fixed color contrast accessibility issues
    
    * Fixed ari-roles issues
    
    * added static storage wip
    
    * Added missing static data
    
    * Update mainCI.yml
    
    * added static data for requests made at publishing time
    
    * updated PublisherStaticDataProvider
    
    * updated PublisherStaticDataProvider
    
    * updated PublisherStaticDataProvider
    
    * get data from default.json too
    
    * added build-static-data script
    
    * refactoring
    
    * removed logs
    
    * removed some headers from static data
    
    * removed headers
    
    * added smaller GraphQL api
    
    * Update mainCI.yml
    
    * Update mainCI.yml
    
    * Update mainCI.yml
    
    * Update mainCI.yml
    
    * test accessibility on profile
    
    * Update mainCI.yml
    
    * Update mainCI.yml
    
    * removed headers
    
    * test with pre-populated e-mail and password
    
    * compacted json files
    
    * removed test e-mail and password
    
    * code review changes
    
    * test for signed in version
    
    * added default.json and defaultMockData.json only for staticData
    
    * fixed webpack configs
    
    * fixed accessibility issues
    
    * fixed accessibility issues
    
    * added staticDataEnviroment constant
    
    * Update mainCI.yml
    
    * removed logs
    
    * renamed defaultMockData.json

---
+++ src/services/staticDataHttpClient.ts
@@ -0,0 +1,91 @@
+import { HttpClient, HttpRequest, HttpResponse, XmlHttpRequestClient } from "@paperbits/common/http";
+import IStaticDataProvider from "./IStaticDataProvider";
+
+
+export class StaticDataHttpClient implements HttpClient {
+    private readonly httpClient: XmlHttpRequestClient;
+    private readonly mockData: string = "defaultStaticData.json";
+    private readonly defaultData: string = "default.json";
+
+    private initPromise: Promise<void>;
+    private mockDataObject: Object;
+    private defaultDataObject: Object;
+
+    constructor(private readonly dataProvider: IStaticDataProvider) {
+        this.httpClient = new XmlHttpRequestClient();
+    }
+
+    private async initialize(): Promise<void> {
+
+        if (this.initPromise) {
+            return this.initPromise;
+        }
+
+        this.mockDataObject = await this.dataProvider.getStaticData(this.mockData);
+        this.defaultDataObject = await this.dataProvider.getStaticData(this.defaultData);
+    }
+
+    private async ensureInitialized(): Promise<void> {
+        if (!this.initPromise) {
+            this.initPromise = this.initialize();
+        }
+        return this.initPromise;
+    }
+
+    public async send<T>(request: HttpRequest): Promise<HttpResponse<T>> {
+        if (request.method === undefined) {
+            request.method = 'GET';
+        }
+
+        let response = new HttpResponse<T>();
+        await this.ensureInitialized();
+        let result: any;
+
+
+        let urlWithoutParameters = request.url.split('?')[0];
+
+        if (request.url.match("contentTypes\/.*\/contentItems\/")) {
+            response.statusCode = 200;
+            response.statusText = "OK";
+            response.headers = [{ name: "content-type", value: "application/json; charset=utf-8" }];
+            response.body = Buffer.from(JSON.stringify(this.defaultDataObject[this.getShortUrl(request.url)]));
+
+            return response;
+        }
+
+        // Create new subscription
+        const regexp = new RegExp(`https:\/\/contoso\.management\.azure-api\.net\/subscriptions\/sid\/resourceGroups\/rgid\/providers\/Microsoft\.ApiManagement\/service\/sid\/users\/6189460d4634612164e10999\/subscriptions\/[a-xA-Z0-9]*$`);
+        const matches = regexp.exec(urlWithoutParameters);
+        
+        if (matches && matches.length == 1) {
+            result = this.mockDataObject["https://contoso.management.azure-api.net/subscriptions/sid/resourceGroups/rgid/providers/Microsoft.ApiManagement/service/sid/users/6189460d4634612164e10999/subscriptions/61fd37461359a02500aad62f"];
+        }
+        else {
+
+            result = (this.mockDataObject[urlWithoutParameters]);
+        }
+
+        if (result == undefined) {
+            throw new Error(`No mock data for: ${urlWithoutParameters}`);
+        }
+
+        response.headers = result.headers;
+
+        // Image static data is not stored as a JSON object
+        if (result.headers && result.headers.find(h => h.name == "content-type" && h.value.indexOf("image") >= 0)) {
+            return this.httpClient.send(request);
+        }
+
+        response.statusCode = result.statusCode;
+        response.statusText = result.statusText;
+        response.body = Buffer.from(JSON.stringify(result.body));
+
+        return response;
+    }
+
+    private getShortUrl(url: string): string {
+        let shortUrl = url.replace("https://contoso.management.azure-api.net/subscriptions/sid/resourceGroups/rgid/providers/Microsoft.ApiManagement/service/sid", "");
+        return shortUrl.split('?')[0];
+    }
+
+}
--- src/startup.runtime.ts
+++ src/startup.runtime.ts
@@ -2,8 +2,7 @@ import { InversifyInjector } from "@paperbits/common/injection";
 import { CoreRuntimeModule } from "@paperbits/core/core.runtime.module";
 import { StyleRuntimeModule } from "@paperbits/styles/styles.runtime.module";
 import { ApimRuntimeModule } from "./apim.runtime.module";
-import * as Constants from "./constants";
-
+import {staticDataEnvironment} from "./../environmentConstants"
 
 const injector = new InversifyInjector();
 injector.bindModule(new CoreRuntimeModule());
@@ -11,6 +10,11 @@ injector.bindModule(new StyleRuntimeModule());
 injector.bindModule(new ApimRuntimeModule());
 
 document.addEventListener("DOMContentLoaded", () => {
+    if (process.env.NODE_ENV === staticDataEnvironment) {
+        // Fake token for testing the authenticated version, it's not valid but it respects the regex and contains an expiry date
+        sessionStorage.setItem("accessToken", "6189460d4634612164e10999&292202041536&FWpHkq3S32Bxrgs+Mq0i8XTe6eOkv7zbABcdKAg1mofztdVAY7E8DUdFWC/7991FMuH5I5W1RfuJ0GzUjVsEqQ==");
+    }
+
     injector.resolve("autostart");
 });
 
