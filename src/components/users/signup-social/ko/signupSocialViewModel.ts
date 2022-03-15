095a98fc4ebac452fb9731fd47d3c0715a6bff9b
Jonas Anker Rasmussen <jonasanker@gmail.com>
2022-03-15 23:53:36 +0100

    Fix AAD/B2C message shown on SignUp page error (#1687)
    Implementation inspired by #1621
    
    Co-authored-by: Jonas Anker Rasmussen <jra@cvation.com>

--- src/components/users/signup-social/ko/signupSocialViewModel.ts
+++ src/components/users/signup-social/ko/signupSocialViewModel.ts
@@ -10,10 +10,12 @@ export class SignupSocialViewModel {
     public readonly identityProvider: ko.Observable<boolean>;
     public readonly mode: ko.Observable<string>;
     public readonly runtimeConfig: ko.Observable<string>;
+    public readonly showNoAadConfigMessage: ko.Computed<boolean>;
 
     constructor() {
         this.identityProvider = ko.observable<boolean>();
         this.mode = ko.observable<string>();
         this.runtimeConfig = ko.observable<string>();
+        this.showNoAadConfigMessage = ko.computed<boolean>(() => !this.identityProvider() && this.mode() !== "publishing"); 
     }
 }
