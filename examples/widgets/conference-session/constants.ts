490ad1f4394ac59020bcd5a848b4ec2cf038f11f
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2022-03-31 17:30:04 -0700

    Create SECURITY.md

---
+++ examples/widgets/conference-session/constants.ts
@@ -0,0 +1,7 @@
+export const widgetName = "conference-session";
+export const widgetDisplayName = "Conference session";
+export const widgetCategory = "Community";
+export const widgetSelector = "conference-session";
+export const widgetRuntimeSelector = "conference-session-runtime";
+export const widgetEditorSelector = "conference-session-editor";
+export const defaultSessionNumber = "107";
---
+++ src/components/custom-html/constants.ts
@@ -0,0 +1,7 @@
+export const widgetName = "custom-html-code";
+export const widgetDisplayName = "Custom HTML code";
+export const widgetCategory = "Advanced";
+export const widgetSelector = "custom-html";
+export const widgetRuntimeSelector = "custom-html-runtime";
+export const widgetEditorSelector = "custom-html-editor";
+export const widgetIconClass = "widget-icon widget-icon-api-management";
