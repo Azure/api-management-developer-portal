383dc5d47b4893b999f312ae3b2d6b183f00cd66
Cristina Malin <malincrist@microsoft.com>
2022-04-04 19:56:46 +0200

    Load api list when the API list dropdown widget is first initialized

--- scaffolds/widget/constants.ts
+++
@@ -1,6 +0,0 @@
-export const widgetName = "your-widget";
-export const widgetDisplayName = "Your widget";
-export const widgetCategory = "Community";
-export const widgetSelector = "widget";
-export const widgetRuntimeSelector = "widget-runtime";
-export const widgetEditorSelector = "widget-editor";
--- tests/constants.ts
+++
@@ -1,7 +0,0 @@
-import { LaunchOptions, BrowserLaunchArgumentOptions, BrowserConnectOptions } from "puppeteer";
-
-export const BrowserLaunchOptions: LaunchOptions & BrowserLaunchArgumentOptions & BrowserConnectOptions = {
-    headless: false,
-    ignoreHTTPSErrors: true,
-    product: "chrome"
-};
