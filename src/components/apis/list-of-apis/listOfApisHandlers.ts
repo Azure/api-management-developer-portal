3c48f5547063618926c92ac593057acb5844f7a5
Cristina Malin <malincrist@microsoft.com>
2022-04-11 16:26:13 +0200

    enable styling for sub-somponents WIP

--- src/components/apis/list-of-apis/listOfApisHandlers.ts
+++ src/components/apis/list-of-apis/listOfApisHandlers.ts
@@ -1,4 +1,5 @@
 ﻿import { IWidgetOrder, IWidgetHandler } from "@paperbits/common/editing";
+import { StyleDefinition } from "@paperbits/common/styles";
 import { ListOfApisModel } from "./listOfApisModel";
 
 export class ListOfApisHandlers implements IWidgetHandler {
@@ -13,6 +14,114 @@ export class ListOfApisHandlers implements IWidgetHandler {
         };
 
         return widgetOrder;
+    }
+
+    public getStyleDefinitions(): StyleDefinition {
+        return {
+            colors: {
+                testColor: {
+                    displayName: "Pink",
+                    defaults: {
+                        value: "pink"
+                    }
+                },
+                testColor2: {
+                    displayName: "Pink",
+                    defaults: {
+                        value: "violet"
+                    }
+                },
+                borderColor: {
+                    displayName: "Border - light gray",
+                    defaults: {
+                        value: "#505050"
+                    }
+                },
+                gridBorderColor:{
+                    displayName: "Grid - border color",
+                    defaults:{
+                        value: "#dee2e6"
+                    }
+                }
+            },
+            components: {
+                listOfApis: {
+                    displayName: "listOfApis",
+                    plugins: ["margin", "padding", "typography"],
+                    components: {
+                        searchInput: {
+                            displayName: "Search input",
+                            plugins: ["typography", "border", "states"],
+                            defaults: {
+                                allowedStates: [
+                                    "hover",
+                                    "focus",
+                                    "active"
+                                ],
+                                typography: {
+                                    fontSize: "1rem",
+                                    colorKey: "colors/default",
+                                    lineHeight: "1.5"
+                                },
+
+                                border: {
+                                    bottom: {
+                                        width: "1",
+                                        style: "solid",
+                                        colorKey: "colors/borderColor"
+                                    }
+                                }
+                            }
+                        },
+                        apisGridRow:{
+                            displayName: "Grid row",
+                            plugins: ["background", "typography","border"],
+                            defaults: {
+                                border: {
+                                    top: {
+                                        width: "1",
+                                        style: "solid",
+                                        colorKey: "colors/gridBorderColor"
+                                    }
+                                },
+                                typography: {
+                                    fontWeight: "normal",
+                                    colorKey: "colors/default"
+                                },
+                                display: "flex",
+                                padding:{
+                                    top: ".75rem",
+                                    bottom: ".75rem"
+                                   
+                                }
+                            }
+                        },
+                        apisGridHeader:{
+                            displayName: "Grid row",
+                            plugins: ["background", "typography","border"],
+                            defaults: {
+                                border: {
+                                    bottom: {
+                                        width: "1",
+                                        style: "solid",
+                                        colorKey: "colors/gridBorderColor"
+                                    }
+                                },
+                                typography: {
+                                    fontWeight: "bold",
+                                    colorKey: "colors/default"
+                                },
+                                display: "flex",
+                                padding:{
+                                    top: ".75rem",
+                                    bottom: ".75rem"
+                                } 
+                            }
+                        }
+                    }
+                }
+            }
+        };
     }
 }
 export class ListOfApisTilesHandlers implements IWidgetHandler {
