c38dc197fa4dba3f62633af9e64a9ef4e4e2ef8e
malincrist <92857141+malincrist@users.noreply.github.com>
2022-04-28 12:56:19 +0300

    Added local styles for the sub-components of the API list widget (#1733)
    * enable styling for sub-somponents WIP
    
    * added styles for label and tag-input
    
    * fixed typo
    
    * Merged paperbits changes. (#1741)
    
    Co-authored-by: Alexander Zaslonov <zaslonov.alexander@gmail.com>

--- src/components/apis/list-of-apis/listOfApisHandlers.ts
+++ src/components/apis/list-of-apis/listOfApisHandlers.ts
@@ -1,4 +1,5 @@
 ﻿import { IWidgetOrder, IWidgetHandler } from "@paperbits/common/editing";
+import { StyleDefinition } from "@paperbits/common/styles";
 import { ListOfApisModel } from "./listOfApisModel";
 
 export class ListOfApisHandlers implements IWidgetHandler {
@@ -13,6 +14,181 @@ export class ListOfApisHandlers implements IWidgetHandler {
         };
 
         return widgetOrder;
+    }
+
+    public getStyleDefinitions(): StyleDefinition {
+        return {
+            colors: {
+                borderColor: {
+                    displayName: "Search input border color",
+                    defaults: {
+                        value: "#505050"
+                    }
+                },
+                gridBorderColor: {
+                    displayName: "Grid separators color",
+                    defaults: {
+                        value: "#dee2e6"
+                    }
+                },
+                tagButtonColor: {
+                    displayName: "Tag button color",
+                    defaults: {
+                        value: "#555"
+                    }
+                }
+            },
+            components: {
+                listOfApis: {
+                    displayName: "List of APIs",
+                    plugins: ["margin", "padding", "typography"],
+                    components: {
+                        searchInput: {
+                            displayName: "Search input",
+                            plugins: ["typography", "border", "states"],
+                            defaults: {
+                                allowedStates: [
+                                    "focus"
+                                ],
+                                typography: {
+                                    fontSize: "1rem",
+                                    colorKey: "colors/default",
+                                    lineHeight: "1.5"
+                                },
+                                border: {
+                                    bottom: {
+                                        width: "1",
+                                        style: "solid",
+                                        colorKey: "colors/borderColor"
+                                    }
+                                },
+                                states: {
+                                    focus: {
+                                        typography: {
+                                            fontSize: "1rem",
+                                            colorKey: "colors/default",
+                                            lineHeight: "1.5"
+                                        },
+                                        border: {
+                                            bottom: {
+                                                width: "1",
+                                                style: "solid",
+                                                colorKey: "colors/borderColor"
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        apisGridRow: {
+                            displayName: "Grid row",
+                            plugins: ["background", "typography", "border"],
+                            defaults: {
+                                border: {
+                                    top: {
+                                        width: "1",
+                                        style: "solid",
+                                        colorKey: "colors/gridBorderColor"
+                                    }
+                                },
+                                typography: {
+                                    fontWeight: "normal",
+                                    colorKey: "colors/default"
+                                },
+                                display: "flex",
+                                padding: {
+                                    top: ".75rem",
+                                    bottom: ".75rem"
+
+                                }
+                            }
+                        },
+                        apisGridHeader: {
+                            displayName: "Grid row",
+                            plugins: ["background", "typography", "border"],
+                            defaults: {
+                                border: {
+                                    bottom: {
+                                        width: "1",
+                                        style: "solid",
+                                        colorKey: "colors/gridBorderColor"
+                                    }
+                                },
+                                typography: {
+                                    fontWeight: "bold",
+                                    colorKey: "colors/default"
+                                },
+                                display: "flex",
+                                padding: {
+                                    top: ".75rem",
+                                    bottom: ".75rem"
+                                }
+                            }
+                        },
+                        tagInput: {
+                            displayName: "tag input",
+                            plugins: ["background", "border", "typography"],
+                            defaults: {
+                                border: {
+                                    bottom: {
+                                        width: 1,
+                                        style: "solid",
+                                        colorKey: "colors/tagButtonColor"
+                                    },
+                                    top: {
+                                        width: 1,
+                                        style: "solid",
+                                        colorKey: "colors/tagButtonColor"
+                                    },
+                                    right: {
+                                        width: 1,
+                                        style: "solid",
+                                        colorKey: "colors/tagButtonColor"
+                                    },
+                                    left: {
+                                        width: 1,
+                                        style: "solid",
+                                        colorKey: "colors/tagButtonColor"
+                                    }
+                                },
+
+                                typography: {
+                                    colorKey: "colors/tagButtonColor",
+                                    textDecoration: "none"
+                                },
+                                padding: {
+                                    top: 2,
+                                    right: 7,
+                                    bottom: 2,
+                                    left: 7
+                                },
+                                margin: {
+                                    top: 2,
+                                    right: 2,
+                                    bottom: 2,
+                                    left: 2
+                                },
+                                size: {
+                                    minHeight: "2em"
+                                },
+                                container: {
+                                    alignItems: "center"
+                                }
+                            }
+                        },
+                        toggleButtonLabel: {
+                            displayName: "Label",
+                            plugins: ["typography"],
+                            defaults: {
+                                typography: {
+                                    colorKey: "colors/default"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        };
     }
 }
 export class ListOfApisTilesHandlers implements IWidgetHandler {
