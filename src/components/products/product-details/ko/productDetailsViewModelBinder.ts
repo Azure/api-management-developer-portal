0bf0423f3122e79cb455db122986cac0ba85c955
malincrist <92857141+malincrist@users.noreply.github.com>
2022-05-05 10:58:25 +0300

    Added local styles for the Product Details Widget (#1754)
    * Added local styles for the Product Details Widget

--- src/components/products/product-details/ko/productDetailsViewModelBinder.ts
+++ src/components/products/product-details/ko/productDetailsViewModelBinder.ts
@@ -3,8 +3,14 @@ import { ProductDetailsViewModel } from "./productDetailsViewModel";
 import { ProductDetailsModel } from "../productDetailsModel";
 import { Bag } from "@paperbits/common";
 import { ComponentFlow } from "@paperbits/common/editing";
+import { StyleCompiler } from "@paperbits/common/styles";
+import { ProductDetailsHandlers } from "../productDetailsHandlers";
+import { EventManager, Events } from "@paperbits/common/events";
 
 export class ProductDetailsViewModelBinder implements ViewModelBinder<ProductDetailsModel, ProductDetailsViewModel> {
+    constructor(private readonly eventManager: EventManager,
+        private readonly styleCompiler: StyleCompiler) { }
+
     public async modelToViewModel(model: ProductDetailsModel, viewModel?: ProductDetailsViewModel, bindingContext?: Bag<any>): Promise<ProductDetailsViewModel> {
         if (!viewModel) {
             viewModel = new ProductDetailsViewModel();
@@ -14,8 +20,17 @@ export class ProductDetailsViewModelBinder implements ViewModelBinder<ProductDet
             displayName: "Product: Details",
             model: model,
             draggable: true,
+            handler: ProductDetailsHandlers,
             flow: ComponentFlow.Block,
+            applyChanges: async (updatedModel: ProductDetailsModel) => {
+                await this.modelToViewModel(updatedModel, viewModel, bindingContext);
+                this.eventManager.dispatchEvent(Events.ContentUpdate);
+            }
         };
+
+        if (model.styles) {
+            viewModel.styles(await this.styleCompiler.getStyleModelAsync(model.styles, bindingContext?.styleManager, ProductDetailsHandlers));
+        }
 
         return viewModel;
     }
