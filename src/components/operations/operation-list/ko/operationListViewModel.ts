7cf29d940263c478c5eb33daaaa047cc643a226f
malincrist <92857141+malincrist@users.noreply.github.com>
2022-05-16 12:03:12 +0300

    Added local styles for operation-list (#1767)
    Added local styles for operation-list

--- src/components/operations/operation-list/ko/operationListViewModel.ts
+++ src/components/operations/operation-list/ko/operationListViewModel.ts
@@ -1,6 +1,7 @@
 import * as ko from "knockout";
 import template from "./operationList.html";
 import { Component } from "@paperbits/common/ko/decorators";
+import { StyleModel } from "@paperbits/common/styles";
 
 @Component({
     selector: "operationList",
@@ -8,8 +9,10 @@ import { Component } from "@paperbits/common/ko/decorators";
 })
 export class OperationListViewModel {
     public readonly runtimeConfig: ko.Observable<string>;
+    public readonly styles: ko.Observable<StyleModel>;
 
     constructor() {
         this.runtimeConfig = ko.observable();
+        this.styles = ko.observable<StyleModel>();
     }
 }
