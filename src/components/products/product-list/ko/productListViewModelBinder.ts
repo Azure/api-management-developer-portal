021093cc2dbf070e9420be9281c4714b4f166b1e
malincrist <92857141+malincrist@users.noreply.github.com>
2022-05-17 12:38:59 +0300

    Added local styles for API List tiles widget + refactoring (#1768)
    * Added local styles for API List tiles widget + refactoring

--- src/components/products/product-list/ko/productListViewModelBinder.ts
+++ src/components/products/product-list/ko/productListViewModelBinder.ts
@@ -7,6 +7,10 @@ import { ComponentFlow } from "@paperbits/common/editing";
 import { StyleCompiler } from "@paperbits/common/styles";
 import { ProductListDropdownHandlers, ProductListHandlers, ProductListTilesHandlers } from "../productListHandlers";
 
+const handlerForLayout = {
+    list: ProductListHandlers,
+    dropdown: ProductListDropdownHandlers
+};
 
 export class ProductListViewModelBinder implements ViewModelBinder<ProductListModel, ProductListViewModel> {
     constructor(
@@ -27,10 +31,7 @@ export class ProductListViewModelBinder implements ViewModelBinder<ProductListMo
                 : undefined
         }));
 
-        let handler = model.layout == "list" ? ProductListHandlers :
-            model.layout == "dropdown" ?
-                ProductListDropdownHandlers :
-                ProductListTilesHandlers;
+        const handler = handlerForLayout[model.layout] ?? ProductListTilesHandlers;
 
         viewModel["widgetBinding"] = {
             displayName: "List of products" + (model.layout === "list" ? "" : ` (${model.layout})`),
