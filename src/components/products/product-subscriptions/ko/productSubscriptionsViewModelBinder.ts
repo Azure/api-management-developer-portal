392b1e99ba334ad75fc08aafad61ca804189cc71
malincrist <92857141+malincrist@users.noreply.github.com>
2022-05-23 17:39:03 +0200

    Added local styles for product subscriptions widget (#1772)
    Added local styles for product subscriptions widget

--- src/components/products/product-subscriptions/ko/productSubscriptionsViewModelBinder.ts
+++ src/components/products/product-subscriptions/ko/productSubscriptionsViewModelBinder.ts
@@ -3,19 +3,34 @@ import { ProductSubscriptionsViewModel } from "./productSubscriptionsViewModel";
 import { ProductSubscriptionsModel } from "../productSubscriptionsModel";
 import { Bag } from "@paperbits/common";
 import { ComponentFlow } from "@paperbits/common/editing";
+import { StyleCompiler } from "@paperbits/common/styles";
+import { ProductSubscriptionsHandlers } from "../productSubscriptionsHandlers";
+import { EventManager, Events } from "@paperbits/common/events";
 
 export class ProductSubscriptionsViewModelBinder implements ViewModelBinder<ProductSubscriptionsModel, ProductSubscriptionsViewModel> {
+    constructor(
+        private readonly eventManager: EventManager,
+        private readonly styleCompiler: StyleCompiler) { }
     public async modelToViewModel(model: ProductSubscriptionsModel, viewModel?: ProductSubscriptionsViewModel, bindingContext?: Bag<any>): Promise<ProductSubscriptionsViewModel> {
         if (!viewModel) {
             viewModel = new ProductSubscriptionsViewModel();
-            
+
             viewModel["widgetBinding"] = {
                 displayName: "Product: Subscriptions",
                 layer: bindingContext?.layer,
                 model: model,
                 flow: ComponentFlow.Block,
-                draggable: true
+                draggable: true,
+                handler: ProductSubscriptionsHandlers,
+                applyChanges: async (updatedModel: ProductSubscriptionsModel) => {
+                    await this.modelToViewModel(updatedModel, viewModel, bindingContext);
+                    this.eventManager.dispatchEvent(Events.ContentUpdate);
+                }
             };
+        }
+
+        if (model.styles) {
+            viewModel.styles(await this.styleCompiler.getStyleModelAsync(model.styles, bindingContext?.styleManager, ProductSubscriptionsHandlers));
         }
 
         return viewModel;
