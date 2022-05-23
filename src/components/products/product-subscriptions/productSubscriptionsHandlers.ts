d91d2086272cdab2e49ce4596407ca74fe189a1a
malincrist <92857141+malincrist@users.noreply.github.com>
2022-05-23 17:39:03 +0200

    Added local styles for product subscriptions widget (#1772)
    Added local styles for product subscriptions widget

--- src/components/products/product-subscriptions/productSubscriptionsHandlers.ts
+++ src/components/products/product-subscriptions/productSubscriptionsHandlers.ts
@@ -1,5 +1,9 @@
 ﻿import { IWidgetOrder, IWidgetHandler } from "@paperbits/common/editing";
+import { StyleDefinition } from "@paperbits/common/styles";
 import { ProductSubscriptionsModel } from "./productSubscriptionsModel";
+import * as DefaultStyleDefinitions from "../../defaultStyleDefinitions";
+import { cloneDeep } from "lodash";
+
 
 export class ProductSubscriptionsHandlers implements IWidgetHandler {
     public async getWidgetOrder(): Promise<IWidgetOrder> {
@@ -13,5 +17,29 @@ export class ProductSubscriptionsHandlers implements IWidgetHandler {
         };
 
         return widgetOrder;
+    }
+
+    public getStyleDefinitions(): StyleDefinition {
+        return {
+            colors: {
+                gridBorderColor: {
+                    displayName: "Grid separators color",
+                    defaults: {
+                        value: "#dee2e6"
+                    }
+                },
+            },
+            components:{
+                productSubscriptions:{
+                    displayName: "Product Subscriptions",
+                    plugins:["background", "typography","margin","padding"],
+                    components:
+                    {
+                        tableRow: cloneDeep(DefaultStyleDefinitions.GridRow),
+                        tableHead: cloneDeep(DefaultStyleDefinitions.GridHeader),
+                    }
+                }
+            }
+        };
     }
 }
