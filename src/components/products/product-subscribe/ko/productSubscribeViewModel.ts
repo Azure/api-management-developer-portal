e2e1664703f94e81abe05e8993e470baea294e23
malincrist <92857141+malincrist@users.noreply.github.com>
2022-05-24 09:43:29 +0200

    Local styles/product subscribe (#1780)
    Added local styles for Product Subscribe Form widget

--- src/components/products/product-subscribe/ko/productSubscribeViewModel.ts
+++ src/components/products/product-subscribe/ko/productSubscribeViewModel.ts
@@ -1,16 +1,18 @@
 import * as ko from "knockout";
 import template from "./productSubscribe.html";
 import { Component } from "@paperbits/common/ko/decorators/component.decorator";
+import { StyleModel } from "@paperbits/common/styles";
 
 @Component({
     selector: "product-subscribe",
     template: template
 })
 export class ProductSubscribeViewModel {
-	public readonly runtimeConfig: ko.Observable<string>;
+    public readonly runtimeConfig: ko.Observable<string>;
+    public readonly styles: ko.Observable<StyleModel>;
 
-	constructor() {        
+    constructor() {
         this.runtimeConfig = ko.observable();
+        this.styles = ko.observable<StyleModel>();
     }
-	
 }
