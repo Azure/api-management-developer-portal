d85a81cda635186dd9945e10a6a22a7102069f38
Cristina Malin <malincrist@microsoft.com>
2022-06-16 17:13:37 +0200

    Added local styles for confirm password widget

--- src/components/users/confirm-password/ko/confirmPasswordViewModel.ts
+++ src/components/users/confirm-password/ko/confirmPasswordViewModel.ts
@@ -1,10 +1,16 @@
 import * as ko from "knockout";
 import template from "./confirmPassword.html";
 import { Component } from "@paperbits/common/ko/decorators";
-
+import { StyleModel } from "@paperbits/common/styles";
 
 @Component({
     selector: "confirmPassword",
     template: template
 })
-export class ConfirmPasswordViewModel {}
+export class ConfirmPasswordViewModel {
+    public readonly styles: ko.Observable<StyleModel>;
+
+    constructor() {
+        this.styles = ko.observable<StyleModel>();
+    }
+}
--- src/components/users/confirm-password/ko/confirmPasswordViewModelBinder.ts
+++ src/components/users/confirm-password/ko/confirmPasswordViewModelBinder.ts
@@ -3,10 +3,18 @@ import { Bag } from "@paperbits/common";
 import { ConfirmPasswordViewModel } from "./confirmPasswordViewModel";
 import { ConfirmPasswordModel } from "../confirmPasswordModel";
 import { ComponentFlow } from "@paperbits/common/editing";
+import { EventManager, Events } from "@paperbits/common/events";
+import { StyleCompiler } from "@paperbits/common/styles";
+import { ConfirmPasswordHandlers } from "../confirmPasswordHandlers";
 
 
 
 export class ConfirmPassworViewModelBinder implements ViewModelBinder<ConfirmPasswordModel, ConfirmPasswordViewModel> {
+    constructor(
+        private readonly eventManager: EventManager,
+        private readonly styleCompiler: StyleCompiler) { }
+
+
     public async modelToViewModel(model: ConfirmPasswordModel, viewModel?: ConfirmPasswordViewModel, bindingContext?: Bag<any>): Promise<ConfirmPasswordViewModel> {
         if (!viewModel) {
             viewModel = new ConfirmPasswordViewModel();
@@ -15,8 +23,17 @@ export class ConfirmPassworViewModelBinder implements ViewModelBinder<ConfirmPas
                 layer: bindingContext?.layer,
                 model: model,
                 flow: ComponentFlow.Block,
-                draggable: true
+                draggable: true,
+                handler: ConfirmPasswordHandlers,
+                applyChanges: async (updatedModel: ConfirmPasswordModel) => {
+                    this.modelToViewModel(updatedModel, viewModel, bindingContext);
+                    this.eventManager.dispatchEvent(Events.ContentUpdate);
+                }
             };
+        }
+
+        if (model.styles) {
+            viewModel.styles(await this.styleCompiler.getStyleModelAsync(model.styles, bindingContext?.styleManager, ConfirmPasswordHandlers));
         }
 
         return viewModel;
