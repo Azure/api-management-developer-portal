81e2f223dafc71a6a2553e5dcc73571afbbc9b6b
Harun Reşit <harun5hy@outlook.com>
2022-06-16 16:50:36 +0300

    converted product service

--- src/contracts/product.ts
+++ src/contracts/product.ts
@@ -1,13 +1,9 @@
-import { ArmResource } from "./armResource";
-
-export interface ProductContract extends ArmResource {
-    properties: {
-        displayName: string;
-        description: string;
-        approvalRequired: boolean;
-        state: string;
-        subscriptionRequired: boolean;
-        subscriptionsLimit: number;
-        terms: string;
-    };
+export interface ProductContract {
+    id: string;
+    name: string;
+    description: string;
+    approvalRequired: boolean;
+    subscriptionRequired: boolean;
+    subscriptionsLimit: number;
+    terms: string;
 }
