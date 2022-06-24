e0054f7edd86bb2083ce715b6da584b3150b79c6
Farhad Alizada <falizada@microsoft.com>
2022-06-24 17:11:53 +0200

    Introduct base developer portal logging

---
+++ src/models/logging/devPortalEvent.ts
@@ -0,0 +1,38 @@
+import { JSONObject } from "puppeteer";
+
+export class DevPortalEvent {
+    /**
+     * Event type.
+     */
+    public eventType: string;
+
+    /**
+     * Event message.
+     */
+    public message: string;
+
+    /**
+     * Payload of event data.
+     */
+    public eventData: string;
+
+    /**
+     * ISO representation of current time.
+     */
+    public timestamp: string;
+
+    /**
+     * uuidv4 value.
+     */
+    public activityId: string;
+
+    public toJson(): JSONObject{
+        return {
+            "eventType": this.eventType,
+            "message": this.message,
+            "eventData": this.eventData,
+            "timestamp": this.timestamp,
+            "activityId": this.activityId
+        }
+    }
+}
