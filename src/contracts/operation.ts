36306fc9bb6ac123683db5673708b9d0fb69ee9c
Harun Reşit <harun5hy@outlook.com>
2022-06-24 15:02:08 +0200

    add comments for contracts

--- src/contracts/operation.ts
+++ src/contracts/operation.ts
@@ -6,14 +6,45 @@ import { ResponseContract } from "./response";
  * Model of API operation
  */
 export interface OperationContract {
+
+    /**
+     * Operation identifier.
+     */
     id: string;
+
+    /**
+     * Operation name.  Must be 1 to 300 characters long.
+     */
     name: string;
+
+    /**
+     * Operation description.
+     */
     description: string;
+
+    /**
+     * Operation URI template. Cannot be more than 400 characters long.
+     */
     urlTemplate: string;
+
+    /**
+     * Collection of URL template parameters.
+     */
     templateParameters: ParameterContract[];
+
+    /**
+     * Operation method. Example: GET, POST, PUT, DELETE, etc.
+     */
     method: string;
+
     backend?: string;
+
     version?: string;
+
     request?: RequestContract;
+
+    /**
+     * Array of Operation responses.
+     */
     responses?: ResponseContract[];
 }
--- src/contracts/subscription.ts
+++ src/contracts/subscription.ts
@@ -1,16 +1,67 @@
 export interface SubscriptionContract {
+    /**
+     * Subscription identifier.
+     */
     id: string;
+
+    /**
+     * Subscription name.
+     */
     name: string;
+
+    /**
+     * Subscription creation date.
+     */
     createdDate: string;
+
+    /**
+     * Date when subscription was cancelled or expired.
+     */
     endDate: string;
+
+    /**
+     * Subscription expiration date.
+     */
     expirationDate: string;
+
+    /**
+     * Upcoming subscription expiration notification date.
+     */
     notificationDate: string;
+
+    /**
+     * Subscription primary key.
+     */
     primaryKey: string;
+
+    /**
+     * Scope like /products/{productId} or /apis or /apis/{apiId} or /.
+     */
     scope: string;
+
+    /**
+     * Subscription secondary key.
+     */
     secondaryKey: string;
+
+    /**
+     * Subscription activation date.
+     */
     startDate: string;
+
+    /**
+     * Subscription state.
+     */
     state: string;
+
+    /**
+     * Subscription State Comment.
+     */
     stateComment: string;
+
+    /**
+     * Owner of the subscription
+     */
     ownerId: string;
 }
 
