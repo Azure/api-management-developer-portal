ce57d338700626726c8314ae20a994e0fd104320
malincrist <92857141+malincrist@users.noreply.github.com>
2022-06-27 11:20:07 +0200

    Added local styles for Reports widget (#1832)
    Added local styles for Reports widget

--- src/components/reports/reportsContract.ts
+++ src/components/reports/reportsContract.ts
@@ -1,3 +1,9 @@
 import { Contract } from "@paperbits/common";
+import { LocalStyles } from "@paperbits/common/styles";
 
-export interface ReportsContract extends Contract { }
+export interface ReportsContract extends Contract {
+    /**
+     * Widget local styles. 
+     */
+    styles?: LocalStyles;
+}
--- src/components/reports/reportsHandlers.ts
+++ src/components/reports/reportsHandlers.ts
@@ -1,5 +1,7 @@
 ﻿import { IWidgetOrder, IWidgetHandler } from "@paperbits/common/editing";
+import { StyleDefinition } from "@paperbits/common/styles";
 import { ReportsModel } from "./reportsModel";
+import * as DefaultStyleDefinitions from "../defaultStyleDefinitions";
 
 export class ReportsHandlers implements IWidgetHandler {
     public async getWidgetOrder(): Promise<IWidgetOrder> {
@@ -13,5 +15,31 @@ export class ReportsHandlers implements IWidgetHandler {
         };
 
         return widgetOrder;
+    }
+
+    public getStyleDefinitions(): StyleDefinition {
+        return {
+            colors: {
+                gridBorderColor: {
+                    displayName: "Grid separators color",
+                    defaults: {
+                        value: "#dee2e6"
+                    }
+                }
+            },
+            components: {
+                reports: {
+                    displayName: "Reports",
+                    plugins: ["background", "margin", "padding"],
+                    components: {
+                        button: DefaultStyleDefinitions.getDefaultButtonStyleDefinition(),
+                        titleText: DefaultStyleDefinitions.getWidgetTextStyleDefinition(),
+                        tablePresetHead: DefaultStyleDefinitions.getTableHeadCellStyleDefinition(),
+                        tablePresetRow: DefaultStyleDefinitions.getTableRowCellStyleDefinition(),
+                        widgetText: DefaultStyleDefinitions.getWidgetTextStyleDefinition(),
+                    }
+                }
+            }
+        };
     }
 }
