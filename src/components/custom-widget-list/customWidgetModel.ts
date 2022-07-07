633c78b257a8ae369217e8ce2986ca7d87cf5d70
Jan Mach <janmach1@microsoft.com>
2022-07-07 17:13:25 +0200

    tech to technology refactor

--- src/components/custom-widget-list/customWidgetModel.ts
+++ src/components/custom-widget-list/customWidgetModel.ts
@@ -2,6 +2,6 @@ import { TScaffoldTech } from "@azure/api-management-custom-widgets-scaffolder";
 
 export class CustomWidgetModel {
     public name: string;
-    public tech: TScaffoldTech | null;
+    public technology: TScaffoldTech | null;
     // public sourceControl: TScaffoldSourceControl;
 }
