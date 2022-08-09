e3a8a930564af6b585ec65b6f400ca05dae86cb2
malincrist <92857141+malincrist@users.noreply.github.com>
2022-08-09 12:41:47 +0200

    Added local styles for operation console (#1894)
    * Added local styles for the operation-console
    
    * Added missing local styles for operation details widget
    
    * Added missing local styles for operation-details: graphql and websocket apis
    
    * Fixed issues with styleable grid item selection.
    
    * Added missing operation console styles
    
    Co-authored-by: Alexander Zaslonov <zaslonov.alexander@gmail.com>

--- src/components/operations/operation-details/operationDetailsHandlers.ts
+++ src/components/operations/operation-details/operationDetailsHandlers.ts
@@ -1,6 +1,7 @@
 ﻿import { IWidgetOrder, IWidgetHandler } from "@paperbits/common/editing";
 import { StyleDefinition } from "@paperbits/common/styles";
 import { OperationDetailsModel } from "./operationDetailsModel";
+import * as DefaultStyleDefinitions from "../../defaultStyleDefinitions";
 
 export class OperationDetailsHandlers implements IWidgetHandler {
     public async getWidgetOrder(): Promise<IWidgetOrder> {
@@ -24,12 +25,48 @@ export class OperationDetailsHandlers implements IWidgetHandler {
                     defaults: {
                         value: "green"
                     }
-                }
+                },
+                borderColor: {
+                    displayName: "Input border color",
+                    defaults: {
+                        value: "#505050"
+                    }
+                },
+                consoleBackground: {
+                    displayName: "Test Console Background",
+                    defaults: {
+                        value: "#fafafa"
+                    }
+                },
+                consoleHighlightBackground: {
+                    displayName: "Test Console Highlight Background",
+                    defaults: {
+                        value: "#f2f2f2"
+                    }
+                },
+                mutedTextColor: {
+                    displayName: "Muted Text Color",
+                    defaults: {
+                        value: "#d1c8b2"
+                    }
+                },
+                tagButtonColor: {
+                    displayName: "Tag button color",
+                    defaults: {
+                        value: "#555"
+                    }
+                },
+                codeSnippetHoverColor: {
+                    displayName: "Code Snippet Hover Color",
+                    defaults: {
+                        value: "#ccc"
+                    }
+                },
             },
             components: {
                 operationDetails: {
                     displayName: "Operation Details",
-                    plugins: ["margin", "padding", "typography", "background"],
+                    plugins: ["margin", "padding", "background"],
                     components: {
                         tryItButton: {
                             displayName: "Try it button",
@@ -75,6 +112,153 @@ export class OperationDetailsHandlers implements IWidgetHandler {
                                             colorKey: "colors/tryItColor",
                                         }
                                     }
+                                }
+                            }
+                        },
+                        titleText: DefaultStyleDefinitions.getWidgetTextStyleDefinition(),
+                        widgetText: DefaultStyleDefinitions.getWidgetTextStyleDefinition(),
+                        tagCard: DefaultStyleDefinitions.getTagCardStyleDefinition(),
+                        tablePresetHead: DefaultStyleDefinitions.getTableHeadCellStyleDefinition(),
+                        tablePresetRow: DefaultStyleDefinitions.getTableRowCellStyleDefinition(),
+                        codeSnippetCommand: {
+                            displayName: "Button",
+                            plugins: ["typography", "states", "border", "background"],
+                            defaults: {
+                                allowedStates: [
+                                    "hover",
+                                    "focus",
+                                    "active",
+                                    "disabled"
+                                ],
+                                typography: {
+                                    colorKey: "colors/default"
+                                },
+                                border: {
+                                    bottom: {
+                                        width: 0
+                                    },
+                                    left: {
+                                        width: 1,
+                                        style: "solid",
+                                        colorKey: "colors/HUebs"
+                                    },
+                                    top: {
+                                        width: 0
+                                    },
+                                    right: {
+                                        width: 0
+                                    }
+                                }
+                            }
+                        },
+
+                        sendButton: DefaultStyleDefinitions.getButtonStyleDefinition(),
+                        connectButton: DefaultStyleDefinitions.getButtonStyleDefinition(),
+                        iconButton: DefaultStyleDefinitions.getIconButtonStyleDefinition(),
+                        collapseButton: DefaultStyleDefinitions.getIconButtonStyleDefinition(),
+                        closeButton: DefaultStyleDefinitions.getIconButtonStyleDefinition(),
+                        refreshButton: DefaultStyleDefinitions.getIconButtonStyleDefinition(),
+                        consoleCodeSnippetCommand: {
+                            displayName: "Button",
+                            plugins: ["typography", "states", "border", "background"],
+                            defaults: {
+                                allowedStates: [
+                                    "hover",
+                                    "focus",
+                                    "active",
+                                    "disabled"
+                                ],
+                                typography: {
+                                    colorKey: "colors/default"
+                                },
+                                border: {
+                                    bottom: {
+                                        width: 0
+                                    },
+                                    left: {
+                                        width: 0
+                                    },
+                                    top: {
+                                        width: 0
+                                    },
+                                    right: {
+                                        width: 0
+                                    }
+                                },
+                                states: {
+                                    hover: {
+                                        background: {
+                                            colorKey: "colors/codeSnippetHoverColor"
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        uploadButton: DefaultStyleDefinitions.getIconButtonStyleDefinition(),
+                        addButton: DefaultStyleDefinitions.getIconButtonStyleDefinition(),
+                        revealButton: {
+                            displayName: "Button",
+                            plugins: ["typography", "states", "border", "background"],
+                            defaults: {
+                                allowedStates: [
+                                    "hover",
+                                    "focus",
+                                    "active",
+                                    "disabled"
+                                ],
+                                border: {
+                                    bottom: {
+                                        width: "1px",
+                                        style: "solid",
+                                        colorKey: "colors/borderColor"
+                                    },
+                                    top: {
+                                        width: "1px",
+                                        style: "solid",
+                                        colorKey: "colors/borderColor"
+                                    },
+                                    left: {
+                                        width: "1px",
+                                        style: "solid",
+                                        colorKey: "colors/borderColor"
+                                    },
+                                    right: {
+                                        width: "1px",
+                                        style: "solid",
+                                        colorKey: "colors/borderColor"
+                                    },
+
+                                },
+                            }
+                        },
+                        consoleText: DefaultStyleDefinitions.getConsoleWidgetTextStyleDefinition(),
+                        consoleTitle: DefaultStyleDefinitions.getWidgetTitleStyleDefinition(),
+                        input: DefaultStyleDefinitions.getConsoleInputStyleDefinition(),
+                        combobox: DefaultStyleDefinitions.getComboboxStyleDefinition(),
+                        operationConsole: {
+                            displayName: "Test Console",
+                            plugins: ["background"],
+                            defaults: {
+                                background: {
+                                    colorKey: "colors/consoleBackground"
+                                }
+                            }
+                        },
+                        panelHighlight: {
+                            displayName: "Console Panel HighLight",
+                            plugins: ["background"],
+                            defaults: {
+                                background: {
+                                    colorKey: "colors/consoleHighlightBackground"
+                                }
+                            }
+                        },
+                        textMuted: {
+                            displayName: "Muted Text",
+                            plugins: ["typography"],
+                            defaults: {
+                                typography: {
+                                    colorKey: "colors/mutedTextColor"
                                 }
                             }
                         }
