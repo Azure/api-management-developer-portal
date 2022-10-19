327ac3c0de13bdcdb76d0a1f565e21757ee90073
Jan Mach <janmach1@microsoft.com>
2022-10-19 17:59:59 +0200

    collapsible tags WiP

--- src/components/operations/operation-list/ko/runtime/operation-list.ts
+++ src/components/operations/operation-list/ko/runtime/operation-list.ts
@@ -27,6 +27,7 @@ export class OperationList {
     public readonly operations: ko.ObservableArray<Operation>;
     public readonly working: ko.Observable<boolean>;
     public readonly groupByTag: ko.Observable<boolean>;
+    public readonly groupTagsCollapsed: ko.Observable<Set<string>>;
     public readonly operationGroups: ko.ObservableArray<TagGroup<Operation>>;
     public readonly pattern: ko.Observable<string>;
     public readonly tags: ko.Observable<Tag[]>;
@@ -56,6 +57,7 @@ export class OperationList {
         this.working = ko.observable(false);
         this.groupByTag = ko.observable(false);
         this.defaultGroupByTagToEnabled = ko.observable(false);
+        this.groupTagsCollapsed = ko.observable(new Set<string>());
         this.pattern = ko.observable();
         this.tags = ko.observable([]);
         this.pageNumber = ko.observable(1);
@@ -249,6 +251,15 @@ export class OperationList {
 
     public async onTagsChange(tags: Tag[]): Promise<void> {
         this.tags(tags);
+    }
+
+    public groupTagCollapseToggle(tag: string): void {
+        const newSet = this.groupTagsCollapsed();
+        if (newSet.has(tag)) newSet.delete(tag);
+        else newSet.add(tag);
+        this.groupTagsCollapsed(newSet);
+
+        console.log(tag, this.groupTagsCollapsed())
     }
 
     @OnDestroyed()
