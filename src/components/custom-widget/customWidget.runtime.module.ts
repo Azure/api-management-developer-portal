180ab067bb9227e43686551c1427442cb1d35c65
Ján Mach <92859870+JMach1@users.noreply.github.com>
2022-11-28 14:10:45 +0100

    Bugfix - Custom widget URL hash sync in published portal (#1993)

--- src/components/custom-widget/customWidget.runtime.module.ts
+++ src/components/custom-widget/customWidget.runtime.module.ts
@@ -1,8 +1,10 @@
 import { IInjectorModule, IInjector } from "@paperbits/common/injection";
+import { CustomWidget } from "./ko/runtime/customWidget";
 import { ListenForSecretsRequests } from "./listenForSecretsRequests";
 
 export class CustomWidgetRuntimeModule implements IInjectorModule {
     public register(injector: IInjector): void {
+        injector.bind("customWidgetScaffoldRuntime", CustomWidget);
         injector.bindToCollection("autostart", ListenForSecretsRequests);
     }
 }
