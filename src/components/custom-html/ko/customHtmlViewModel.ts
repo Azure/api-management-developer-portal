8ba4106da038afbb450aea2ffc7f088b97dabb97
Ján Mach <92859870+JMach1@users.noreply.github.com>
2023-01-03 20:35:04 +0100

    Custom widgets iframes media & clipboard access allowed (#2050)
    * clipboard-read; clipboard-write
    
    * camera; microphone
    
    * refactor to separate const

--- src/components/custom-html/ko/customHtmlViewModel.ts
+++ src/components/custom-html/ko/customHtmlViewModel.ts
@@ -3,7 +3,7 @@ import template from "./customHtmlView.html";
 import { widgetSelector } from "../constants";
 import { Component } from "@paperbits/common/ko/decorators";
 import { StyleModel } from "@paperbits/common/styles";
-import { iframeSandboxAllows } from "../../../constants";
+import { iframeAllows, iframeSandboxAllows } from "../../../constants";
 
 @Component({
     selector: widgetSelector,
@@ -12,6 +12,7 @@ import { iframeSandboxAllows } from "../../../constants";
 export class CustomHtmlViewModel {
     public readonly styles: ko.Observable<StyleModel>;
     public readonly htmlCode: ko.Observable<string>;
+    public readonly iframeAllows: string = iframeAllows;
     public readonly iframeSandboxAllows: string = iframeSandboxAllows;
 
     constructor() {
