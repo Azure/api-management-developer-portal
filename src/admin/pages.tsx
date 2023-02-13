0334e9d80f3153d54a1a710491f33b0cea0030dc
Elena Shorohova <jsorohova@microsoft.com>
2023-02-13 15:20:18 +0200

    Navigation top level

--- src/admin/pages.tsx
+++ src/admin/pages.tsx
@@ -1,24 +1,55 @@
 import * as React from "react";
-import { IPageService } from "@paperbits/common/pages";
+import { IPageService, PageContract } from "@paperbits/common/pages";
+import { ViewManager, View } from "@paperbits/common/ui";
 import { Query, Operator, Page } from "@paperbits/common/persistence";
-import { Resolve } from "@paperbits/react/decorators"
+import { Resolve } from "@paperbits/react/decorators";
+import { Router } from "@paperbits/common/routing";
 
 
-export class Pages extends React.Component<{}, {}> {
+export class Pages extends React.Component<{}, { pages: PageContract[], currentPage: string }> {
     @Resolve("pageService")
     public pageService: IPageService;
 
+    @Resolve
+    public router: Router;
+    //public viewManager: ViewManager;
+
     constructor(props) {
         super(props);
+
+        this.state = {
+            pages: [],
+            currentPage: '/'
+        }
+    }
+
+    componentDidMount(): void {
+        this.loadPages()
+        //his.setState({ pages: this.loadPages() })
+    }
+
+    componentDidUpdate(prevProps: Readonly<{}>, prevState: Readonly<{ pages: PageContract[]; currentPage: string; }>, snapshot?: any): void {
+        if (prevState.currentPage !== this.state.currentPage) {
+            this.router.navigateTo(this.state.currentPage)
+        }
     }
 
     loadPages = async () => {
         const query = Query.from().orderBy("title");
         const pagesSearchResult = await this.pageService.search(query);
-        console.log(pagesSearchResult);
+        this.setState({ pages: pagesSearchResult.value })
+
+        return;
+    }
+
+    navigateToPage = async (link) => {
+        console.log(link);
+        return await this.router.navigateTo(link)
     }
 
     render() {
-        return <button onClick={this.loadPages}>Load pages</button>
+        return <div>
+            {this.state.pages.map(page => <div onClick={ () => this.setState({ currentPage: page.permalink }) }>{page.title}</div>)}
+        </div>
     }
 }
