01380eeb0cd003ff7d97a74a290031f5fffdd1c6
f-alizada <104755925+f-alizada@users.noreply.github.com>
2023-02-20 14:02:33 +0100

    Extend test widget functionality (#2109)
    Co-authored-by: Farhad Alizada <falizada@microsoft.com>

--- tests/e2e/maps/products.ts
+++ tests/e2e/maps/products.ts
@@ -7,4 +7,10 @@ export class ProductseWidget {
         await this.page.waitForSelector("product-list-runtime.block");
         await this.page.waitForSelector("product-list-runtime div.table div.table-body div.table-row");
     }
+
+    public async getProductsCount(): Promise<number | undefined> {
+        return await this.page.evaluate(() =>
+            document.querySelector("product-list-runtime div.table div.table-body div.table-row")?.parentElement?.childElementCount
+        );
+    }
 }
