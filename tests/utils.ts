26904e39ba956181901447f431f59cfcf091f3fd
f-alizada <104755925+f-alizada@users.noreply.github.com>
2023-02-20 13:32:48 +0100

    Extend api list with two apis. Add randomizer (#2108)
    Co-authored-by: Farhad Alizada <falizada@microsoft.com>

--- tests/utils.ts
+++ tests/utils.ts
@@ -26,6 +26,18 @@ export class Utils {
         return sasToken;
     }
 
+    public static randomIdentifier(length: number = 8): string {
+        let result = "";
+        const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        const charactersLength = characters.length;
+
+        for (let i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+
+        return result;
+    }
+
     public static async createMockServer(responses?: Object[]) {
         var obj = {};
         if (responses?.length){
