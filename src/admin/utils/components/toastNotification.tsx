62475bc7dc483180a01ea13d93e8df0b84d07eff
Elena Shorohova <jsorohova@microsoft.com>
2023-06-12 16:43:22 +0300

    Toast notifications

---
+++ src/admin/utils/components/toastNotification.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { Icon, Stack, Text } from '@fluentui/react';
+import { lightTheme } from '../themes';
+
+interface ToastNotificationProps {
+    title: string,
+    description: string
+}
+
+export class ToastNotification extends React.Component<ToastNotificationProps, {}> {
+    constructor(props: ToastNotificationProps) {
+        super(props);
+    }
+
+    render(): JSX.Element {
+        return <>
+            <Stack horizontal verticalAlign="center">
+                <Icon
+                    iconName="SkypeCircleCheck"
+                    styles={{ root: { color: lightTheme.callingPalette.green } }}
+                />
+                <Text styles={{ root: { fontWeight: 'bold', marginLeft: 8 } }}>{this.props.title}</Text>
+            </Stack>
+            <Text
+                block
+                styles={{ root: { color: lightTheme.palette.neutralSecondary, marginTop: 5 } }}
+            >{this.props.description}</Text>
+        </>
+    };
+}
