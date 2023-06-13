475ff45d4ac9213002e7a8a42caa41f83bec7448
Farhad Alizada <falizada@microsoft.com>
2023-06-13 13:50:05 +0200

    Specify the host to run

--- tests/utils.ts
+++ tests/utils.ts
@@ -90,8 +90,11 @@ export class Utils {
             })
         });
 
-        server.listen(8181, function(){
+        server.listen(8181,"127.0.0.1", function(){
             server.emit("ready");
+            var host = server.address().address;
+            var port = server.address().port;
+            console.log("Example app listening at http://%s:%s", host, port)
         });
     }
 }
