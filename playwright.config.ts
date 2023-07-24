0931d12605523970f1eaea12fc025f2976674e53
Farhad Alizada <falizada@microsoft.com>
2023-07-24 13:30:40 +0200

    rewrite the test image

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testIgnore: 'playwright/*',
-  //timeout: 30_000,
+  //timeout: 50_000,
   use: {
     video: 'retain-on-failure'
   }
