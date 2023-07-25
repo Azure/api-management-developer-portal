e494582a8981b08902a78e8b9eda8a5804af3ae0
Farhad Alizada <falizada@microsoft.com>
2023-07-25 16:07:57 +0200

    Add retries for the e2e test

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testIgnore: 'playwright/*',
   //timeout: 50_000,
+  retries: 2,
   use: {
     video: 'retain-on-failure'
   }
