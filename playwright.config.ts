5c16372e0aea478939f490ba04c8405d0acda4e7
Farhad Alizada <falizada@microsoft.com>
2023-08-05 16:42:55 +0200

    Address PR comments

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  testIgnore: 'playwright/*',
-  //timeout: 50_000,
   retries: 2,
   use: {
     video: 'retain-on-failure'
