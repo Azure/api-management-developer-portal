95f57224e6ad823f62e6f2c00406221646436792
Igor O <ygrik@hotmail.com>
2023-09-25 14:56:40 -0700

    Added logic to decompress request body if it has gzip encoding header (#2297)

--- src/models/knownHttpHeaders.ts
+++ src/models/knownHttpHeaders.ts
@@ -2,6 +2,7 @@ export enum KnownHttpHeaders {
     Authorization = "Authorization",
     CacheControl = "Cache-Control",
     ContentType = "Content-Type",
+    ContentEncoding = "Content-Encoding",
     OcpApimSubscriptionKey = "Ocp-Apim-Subscription-Key",
     OcpApimTrace = "Ocp-Apim-Trace",
     OcpApimTraceLocation = "Ocp-Apim-Trace-Location",
