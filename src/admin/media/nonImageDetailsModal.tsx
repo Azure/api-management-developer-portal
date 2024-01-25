0e0db54c04ea00949bc901d8cff6670a45f3b9f8
Elena Shorohova <92329687+jsorohova@users.noreply.github.com>
2024-01-25 15:51:05 +0100

    Fixed Link media functionality (#2377)

--- src/admin/media/nonImageDetailsModal.tsx
+++ src/admin/media/nonImageDetailsModal.tsx
@@ -16,6 +16,7 @@ interface NonImageDetailsModalState {
 
 interface NonImageDetailsModalProps {
     mediaItem: MediaContract,
+    isLinking?: boolean,
     onDismiss: () => void
 }
 
@@ -65,6 +66,19 @@ export class NonImageDetailsModal extends React.Component<NonImageDetailsModalPr
         });
     }
 
+    onReferenceUrlChange = (): void => {
+        if (!this.state.errors['downloadUrl'] && this.state.mediaItem.fileName === 'media.svg') {
+            const newName = this.state.mediaItem.downloadUrl.split("/").pop();
+            this.setState({
+                mediaItem: {
+                    ...this.state.mediaItem,
+                    fileName: newName,
+                    permalink: '/content/' + newName
+                }
+            })
+        }
+    } 
+
     validatePermalink = async (permalink: string): Promise<string> => {
         if (permalink === this.props.mediaItem?.permalink) return '';
 
@@ -83,8 +97,6 @@ export class NonImageDetailsModal extends React.Component<NonImageDetailsModalPr
 
             return;
         }
-
-        return;
 
         await this.mediaService.updateMedia(this.state.mediaItem);
         this.eventManager.dispatchEvent('onSaveChanges');
@@ -108,7 +120,13 @@ export class NonImageDetailsModal extends React.Component<NonImageDetailsModalPr
                         />
                         <DefaultButton
                             text="Discard"
-                            onClick={this.props.onDismiss}
+                            onClick={async () => {
+                                if (this.props.isLinking) {
+                                    await this.mediaService.deleteMedia(this.state.mediaItem);
+                                    this.eventManager.dispatchEvent('onSaveChanges');
+                                }
+                                this.props.onDismiss();
+                            }}
                         />
                     </Stack>
                 </Stack>
@@ -129,11 +147,22 @@ export class NonImageDetailsModal extends React.Component<NonImageDetailsModalPr
                         styles={textFieldStyles}
                         required
                     />
-                    <CopyableTextField
-                        fieldLabel="Reference URL"
-                        showLabel={true}
-                        copyableValue={this.state.mediaItem.downloadUrl}
-                    />
+                    {this.props.isLinking
+                        ? <TextField
+                            label="Reference URL"
+                            value={this.state.mediaItem.downloadUrl}
+                            onChange={(event, newValue) => this.onInputChange('downloadUrl', newValue, URL_REQUIRED)}
+                            errorMessage={this.state.errors['downloadUrl'] ?? ''}
+                            styles={textFieldStyles}
+                            required
+                            onBlur={() => this.onReferenceUrlChange()}
+                        />
+                        : <CopyableTextField
+                            fieldLabel="Reference URL"
+                            showLabel={true}
+                            copyableValue={this.state.mediaItem.downloadUrl}
+                        />
+                    }
                     <TextField
                         label="Description"
                         multiline
