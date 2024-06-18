2a377377c59b823c3249ef9e08d269b9d4a823e3
Ján Mach <92859870+JMach1@users.noreply.github.com>
2024-06-18 16:31:39 +0200

    FUI - Product details (#2513)

---
+++ src/components/products/product-details/productDetails.runtime.module.ts
@@ -0,0 +1,10 @@
+import { IInjector, IInjectorModule } from "@paperbits/common/injection";
+import { registerCustomElement } from "@paperbits/react/customElements";
+import { ProductDetailsRuntime } from "./react/ProductDetailsRuntime";
+
+export class ProductDetailsRuntimeModule implements IInjectorModule {
+    public register(injector: IInjector): void {
+        injector.bind("ProductDetailsRuntimeModule", ProductDetailsRuntime);
+        registerCustomElement(ProductDetailsRuntime, "fui-product-details-runtime", injector);
+    }
+}
