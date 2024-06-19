28ca2443f7987cbd58f2d5792a2caeedfe259ce3
Jan Mach <janmach1@microsoft.com>
2024-06-19 14:26:29 +0200

    browser specific iframe sandbox tokens support

--- src/constants.ts
+++ src/constants.ts
@@ -316,7 +316,8 @@ export const iframeAllows = "clipboard-read; clipboard-write; camera; microphone
 /**
  * List of allowed attributes for a sandboxed iframe.
  */
-export const iframeSandboxAllows = "allow-same-origin allow-scripts allow-modals allow-forms allow-popups allow-popups-to-escape-sandbox allow-top-navigation allow-pointer-lock";
+export const iframeSandboxAllows = "allow-scripts allow-modals allow-forms allow-popups allow-popups-to-escape-sandbox allow-top-navigation allow-pointer-lock";
+export const iframeSandboxAllowsBrowserSpecific = "allow-downloads allow-presentation allow-orientation-lock"
 
 /**
  * List of downloadable content types
