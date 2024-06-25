a743e0d10c0dbffc0bf427635ffb5c9fed928b0d
Ján Mach <92859870+JMach1@users.noreply.github.com>
2024-06-25 17:48:04 +0200

    FUI - Sign in (#2518)
    * FUI - signin
    
    * sign in fixes

---
+++ src/components/users/signin/react/SignInRuntime.tsx
@@ -0,0 +1,95 @@
+import * as React from "react";
+import { useEffect, useState } from "react";
+import { FluentProvider, Spinner } from "@fluentui/react-components";
+import { Resolve } from "@paperbits/react/decorators";
+import { Router } from "@paperbits/common/routing";
+import { EventManager } from "@paperbits/common/events";
+import * as Constants from "../../../../constants";
+import { UsersService } from "../../../../services";
+import { RouteHelper } from "../../../../routing/routeHelper";
+import { Utils } from "../../../../utils";
+import { SignInForm, THandleSignIn } from "./SignInForm";
+
+type SignInRuntimeProps = {
+    delegationUrl: string
+    termsEnabled: boolean
+    termsOfUse: string
+    isConsentRequired: boolean
+}
+type SignInRuntimeFCProps = SignInRuntimeProps & {
+    usersService: UsersService
+    eventManager: EventManager
+    handleSignIn: THandleSignIn
+};
+
+const initUser = async (usersService: UsersService, redirectUrl: string) => {
+    let userId: string;
+    try {
+        userId = await usersService.getCurrentUserId();
+    } catch (error) {
+        // those errors are expected
+        if (error.code !== "Unauthorized" && error.code !== "ResourceNotFound") throw error;
+    }
+
+    if (userId) {
+        usersService.navigateToHome();
+    } else if (redirectUrl) {
+        location.assign(redirectUrl);
+    }
+};
+
+const ProductSubscribeRuntimeFC = ({ usersService, eventManager, handleSignIn, delegationUrl }: SignInRuntimeFCProps) => {
+    const [working, setWorking] = useState(true);
+
+    useEffect(() => {
+        setWorking(true);
+        initUser(usersService, delegationUrl)
+            .finally(() => setWorking(false));
+    }, [usersService, delegationUrl]);
+
+    if (working) return <Spinner label={"Loading current user"} labelPosition="below" />;
+
+    return (
+        <SignInForm eventManager={eventManager} handleSignIn={handleSignIn}  />
+    );
+};
+
+export class SignInRuntime extends React.Component<SignInRuntimeProps> {
+    @Resolve("usersService")
+    public usersService: UsersService;
+
+    @Resolve("eventManager")
+    public eventManager: EventManager;
+
+    @Resolve("routeHelper")
+    public routeHelper: RouteHelper;
+
+    @Resolve("router")
+    public router: Router;
+
+    handleSignIn = async (email: string, password: string) => {
+        const clientReturnUrl = sessionStorage.getItem("returnUrl");
+        const returnUrl = this.routeHelper.getQueryParameter("returnUrl") || clientReturnUrl;
+
+        await this.usersService.signInWithBasic(email, password);
+
+        if (returnUrl) {
+            return await this.router.navigateTo(Utils.sanitizeReturnUrl(returnUrl));
+        } else {
+            return this.usersService.navigateToHome();
+        }
+    }
+
+    render() {
+        return (
+            <FluentProvider theme={Constants.fuiTheme}>
+                <ProductSubscribeRuntimeFC
+                    {...this.props}
+                    usersService={this.usersService}
+                    eventManager={this.eventManager}
+                    handleSignIn={this.handleSignIn.bind(this)}
+                />
+            </FluentProvider>
+        );
+    }
+}
