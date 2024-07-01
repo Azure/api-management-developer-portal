ed0196f1834b254f11ca1ae43adbecf610e8d3b3
Ján Mach <92859870+JMach1@users.noreply.github.com>
2024-07-01 14:11:39 +0200

    FUI - pagination "hasNextPage" variant added (#2524)

--- src/components/utils/react/Pagination.tsx
+++ src/components/utils/react/Pagination.tsx
@@ -10,28 +10,34 @@ import {
 } from "@fluentui/react-icons";
 
 export type PaginationProps = {
-    pageNumber: number,
-    setPageNumber: Dispatch<SetStateAction<number>>,
-    pageMax: number
-}
+    pageNumber: number
+    setPageNumber: Dispatch<SetStateAction<number>>
+} & ({ pageMax: number } | { hasNextPage: boolean })
 
-export const Pagination = ({pageNumber, setPageNumber, pageMax}: PaginationProps) => (
-    pageMax < 2 ? <></> : (
+export const Pagination = ({ pageNumber, setPageNumber, ...props }: PaginationProps) => (
+    ("pageMax" in props ? props.pageMax < 2 : (!props.hasNextPage && pageNumber < 2)) ? <></> : (
         <Stack horizontal tokens={{childrenGap: ".5rem"}}>
             <Stack.Item>
                 <Button appearance="transparent" onClick={() => setPageNumber(1)} disabled={pageNumber === 1} icon={<ChevronDoubleLeft20Regular />}/>
                 <Button appearance="transparent" onClick={() => setPageNumber(prev => prev - 1)} disabled={pageNumber === 1} icon={<ChevronLeft20Regular />}/>
             </Stack.Item>
 
-            {Array.from({length: pageMax}, (_, i) => i + 1).map(e => (
+            {"pageMax" in props && Array.from({length: props.pageMax}, (_, i) => i + 1).map(e => (
                 <Button key={e} appearance="transparent" onClick={() => setPageNumber(e)} size="small" icon={
                     pageNumber === e ? <b>{e}</b> : <>{e}</>
                 }/>
             ))}
 
             <Stack.Item>
-                <Button appearance="transparent" onClick={() => setPageNumber(prev => prev + 1)} disabled={pageNumber === pageMax} icon={<ChevronRight20Regular />}/>
-                <Button appearance="transparent" onClick={() => setPageNumber(pageMax)} disabled={pageNumber === pageMax} icon={<ChevronDoubleRight20Regular />}/>
+                <Button
+                    appearance="transparent"
+                    onClick={() => setPageNumber(prev => prev + 1)}
+                    disabled={"pageMax" in props ? pageNumber === props.pageMax : !props.hasNextPage}
+                    icon={<ChevronRight20Regular/>}
+                />
+                {"pageMax" in props && (
+                    <Button appearance="transparent" onClick={() => setPageNumber(props.pageMax)} disabled={pageNumber === props.pageMax} icon={<ChevronDoubleRight20Regular />}/>
+                )}
             </Stack.Item>
         </Stack>
     )
