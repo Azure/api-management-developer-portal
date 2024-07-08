cdcdd8f0e589a687a0c75e865a2b1031b5920fe3
Ján Mach <92859870+JMach1@users.noreply.github.com>
2024-07-08 13:16:57 +0200

    FUI - Sign Up + Captcha (#2529)

--- src/components/products/product-subscribe/react/ProductSubscribeForm.tsx
+++ src/components/products/product-subscribe/react/ProductSubscribeForm.tsx
@@ -1,15 +1,9 @@
 import * as React from "react";
 import { useState } from "react";
 import { Stack } from "@fluentui/react";
-import {
-    Checkbox,
-    Field,
-    Input,
-    Link,
-    Textarea,
-} from "@fluentui/react-components";
-import { ChevronDown12Regular, ChevronUp12Regular } from "@fluentui/react-icons";
+import { Input } from "@fluentui/react-components";
 import { BtnSpinner } from "../../../utils/react/BtnSpinner";
+import { TermsOfUse } from "../../../utils/react/TermsOfUse";
 
 export type TSubscribe = (
     subscriptionName: string,
@@ -18,23 +12,13 @@ export type TSubscribe = (
 
 type Props = {
     subscribe: TSubscribe;
-    tos: string | undefined;
+    termsOfUse: string | undefined;
     showTermsByDefault: boolean;
 };
 
-export const ProductSubscribeForm = ({ subscribe, tos, showTermsByDefault }: Props) => {
+export const ProductSubscribeForm = ({ subscribe, termsOfUse, showTermsByDefault }: Props) => {
     const [subscriptionName, setSubscriptionName] = React.useState("");
     const [consented, setConsented] = useState(false);
-    const [showToS, setShowToS] = useState(showTermsByDefault ?? false);
-
-    const tosLabel = (
-        <>
-            I agree to the{" "}
-            <Link as="button" onClick={() => setShowToS((v) => !v)}>
-                Terms of Use {showToS ? <ChevronUp12Regular /> : <ChevronDown12Regular />}
-            </Link>
-        </>
-    );
 
     return (
         <>
@@ -55,32 +39,20 @@ export const ProductSubscribeForm = ({ subscribe, tos, showTermsByDefault }: Pro
                     <BtnSpinner
                         onClick={() => subscribe(subscriptionName, consented)}
                         appearance="primary"
-                        disabled={!subscriptionName || (tos && !consented)}
+                        disabled={!subscriptionName || (termsOfUse && !consented)}
                     >
                         Subscribe
                     </BtnSpinner>
                 </Stack.Item>
             </Stack>
 
-            {tos && (
-                <Checkbox
-                    checked={consented}
-                    onChange={(_, { checked }) => setConsented(!!checked)}
-                    label={tosLabel}
+            {termsOfUse && (
+                <TermsOfUse
+                    termsOfUse={termsOfUse}
+                    consented={consented}
+                    setConsented={setConsented}
+                    showTermsByDefault={showTermsByDefault}
                 />
-            )}
-
-            {showToS && tos && (
-                <Field>
-                    <Textarea
-                        value={tos}
-                        size="small"
-                        resize="vertical"
-                        textarea={{
-                            style: { height: "50vh", maxHeight: "unset" },
-                        }}
-                    />
-                </Field>
             )}
         </>
     );
