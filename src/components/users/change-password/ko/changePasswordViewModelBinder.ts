156e0cb55d1fabddb090a39e2d219e7f12777613
Elena Shorohova <jsorohova@microsoft.com>
2024-09-18 18:24:03 +0300

    FUI - Change password page

--- src/components/users/change-password/ko/changePasswordViewModelBinder.ts
+++ src/components/users/change-password/ko/changePasswordViewModelBinder.ts
@@ -1,18 +1,22 @@
 import { ISettingsProvider } from "@paperbits/common/configuration";
 import { StyleCompiler } from "@paperbits/common/styles";
+import { ISiteService } from "@paperbits/common/sites";
 import { ViewModelBinder, WidgetState } from "@paperbits/common/widgets";
 import { ChangePasswordModel } from "../changePasswordModel";
 import { ChangePasswordViewModel } from "./changePasswordViewModel";
-
+import { isRedesignEnabledSetting } from "../../../../constants";
 
 export class ChangePasswordViewModelBinder implements ViewModelBinder<ChangePasswordModel, ChangePasswordViewModel> {
     constructor(
         private readonly settingsProvider: ISettingsProvider,
-        private readonly styleCompiler: StyleCompiler
+        private readonly styleCompiler: StyleCompiler,
+        private readonly siteService: ISiteService,
     ) { }
 
     public stateToInstance(state: WidgetState, componentInstance: ChangePasswordViewModel): void {
         componentInstance.styles(state.styles);
+        componentInstance.isRedesignEnabled(state.isRedesignEnabled);
+
         componentInstance.runtimeConfig(JSON.stringify({
             requireHipCaptcha: state.requireHipCaptcha
         }));
@@ -20,10 +24,12 @@ export class ChangePasswordViewModelBinder implements ViewModelBinder<ChangePass
 
     public async modelToState(model: ChangePasswordModel, state: WidgetState): Promise<void> {
         const useHipCaptcha = await this.settingsProvider.getSetting<boolean>("useHipCaptcha");
-        state.requireHipCaptcha = useHipCaptcha === undefined ? true : useHipCaptcha
+        state.requireHipCaptcha = useHipCaptcha === undefined ? true : useHipCaptcha;
 
         if (model.styles) {
             state.styles = await this.styleCompiler.getStyleModelAsync(model.styles);
         }
+
+        state.isRedesignEnabled = !!(await this.siteService.getSetting(isRedesignEnabledSetting));
     }
 }
