492eec22d9b0473dfe3342e57e43e605c738ef8b
Igor O <ygrik@hotmail.com>
2024-10-24 15:49:10 -0700

    Fixed sign up social widget

--- src/components/react.components.d.ts
+++ src/components/react.components.d.ts
@@ -1,10 +1,13 @@
 declare namespace JSX {
     interface IntrinsicElements {
+        "fui-signup-aad-runtime": any;
+        "signup-social-runtime": any;
         "fui-signup-runtime": any;
         "signup-runtime": any;
         "fui-validation-summary": any;
         "validation-summary": any;
         "fui-signin-runtime": any;
         "signin-runtime": any;
+        "placeholder-content": any;
     }
 }
