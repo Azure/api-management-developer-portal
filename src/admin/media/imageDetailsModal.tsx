25ead0eaf8f6a63fd8e3d4242d62dc5a60e31cdd
Elena Shorohova <92329687+jsorohova@users.noreply.github.com>
2024-10-25 19:53:59 +0300

    Accessibility fixes for new Admin UI (#2710)

--- src/admin/media/imageDetailsModal.tsx
+++ src/admin/media/imageDetailsModal.tsx
@@ -145,7 +145,7 @@ export class ImageDetailsModal extends React.Component<ImageDetailsModalProps, I
                 containerClassName="admin-modal media-details-modal"
             >
                 <Stack horizontal horizontalAlign="space-between" verticalAlign="center" className="admin-modal-header">
-                    <Text block nowrap className="admin-modal-header-text">Media / { this.state.mediaItem.fileName }</Text>
+                    <Text as="h2" block nowrap className="admin-modal-header-text">Media / { this.state.mediaItem.fileName }</Text>
                     <Stack horizontal tokens={{ childrenGap: 20 }}>
                         <PrimaryButton
                             text="Save"
