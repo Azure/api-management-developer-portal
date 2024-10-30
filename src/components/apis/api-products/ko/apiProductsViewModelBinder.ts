990621f4dd18bad399268f7c482d3ec02e4ce335
Elena Shorohova <92329687+jsorohova@users.noreply.github.com>
2024-10-31 00:22:27 +0200

    FUI - API products (#2712)

--- src/components/apis/api-products/ko/apiProductsViewModelBinder.ts
+++ src/components/apis/api-products/ko/apiProductsViewModelBinder.ts
@@ -1,13 +1,23 @@
 import { ViewModelBinder, WidgetState } from "@paperbits/common/widgets";
+import { ISiteService } from "@paperbits/common/sites/ISiteService";
 import { ApiProductsModel } from "../apiProductsModel";
 import { ApiProductsViewModel } from "./apiProductsViewModel";
+import { isRedesignEnabledSetting } from "../../../../constants";
+import { layoutsMap } from "../../../utils/react/TableListInfo";
 
 export class ApiProductsViewModelBinder implements ViewModelBinder<ApiProductsModel, ApiProductsViewModel> {
+    constructor(
+        private readonly siteService: ISiteService,
+    ) {}
+
     public stateToInstance(state: WidgetState, componentInstance: ApiProductsViewModel): void {
         componentInstance.layout(state.layout);
         componentInstance.runtimeConfig(JSON.stringify({
-            detailsPageUrl: state.detailsPageUrl
+            detailsPageUrl: state.detailsPageUrl,
+            layoutDefault: layoutsMap[state.layout]
         }));
+
+        componentInstance.isRedesignEnabled(state.isRedesignEnabled);
     }
 
     public async modelToState(model: ApiProductsModel, state: WidgetState): Promise<void> {
@@ -15,5 +25,7 @@ export class ApiProductsViewModelBinder implements ViewModelBinder<ApiProductsMo
         state.detailsPageUrl = model.detailsPageHyperlink
             ? model.detailsPageHyperlink.href
             : undefined
+
+        state.isRedesignEnabled = !!(await this.siteService.getSetting(isRedesignEnabledSetting));
     }
 }
